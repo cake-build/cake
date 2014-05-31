ec24ae17d2db71953f2f290c8e9e41920a5e260f
Patrik Svensson <patrik@patriksvensson.se>
2014-06-01 01:41:52 +0200

    Added convenience methods for criterias.

---
+++ src/Cake.Core/Extensions/CakeTaskExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cake.Core.Extensions
+{
+    public static class CakeTaskExtensions
+    {
+        public static CakeTask WithCriteria(this CakeTask task, Func<bool> criteria)
+        {
+            return task.WithCriteria(context => criteria());
+        }
+
+        public static CakeTask WithCriteria(this CakeTask task, bool criteria)
+        {
+            return task.WithCriteria(context => criteria);
+        }
+    }
+}
---
+++ src/Cake.Tests/Extensions/CakeTaskExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cake.Core;
+using Cake.Core.Extensions;
+using Xunit;
+
+namespace Cake.Tests.Extensions
+{
+    public sealed class CakeTaskExtensionsTests
+    {
+        public sealed class TheWithCriteriaMethod
+        {
+            public sealed class ThatAcceptsFuncLambdaWithoutContext
+            {
+                [Fact]
+                public void Should_Evaluate_Criteria()
+                {
+                    // Given
+                    var result = new List<string>();
+                    var engine = new CakeEngine();
+                    engine.Task("A").Does(x => result.Add("A"));
+                    engine.Task("B").IsDependentOn("A").WithCriteria(() => false).Does(x => result.Add("B"));
+                    engine.Task("C").IsDependentOn("B").Does(x => result.Add("C"));
+
+                    // When
+                    engine.Run("C");
+
+                    // Then
+                    Assert.Equal(2, result.Count);
+                    Assert.Equal("A", result[0]);
+                    Assert.Equal("C", result[1]);
+                }      
+            }
+
+            public sealed class ThatAcceptsBoolean
+            {
+                [Fact]
+                public void Should_Evaluate_Criteria()
+                {
+                    // Given
+                    var result = new List<string>();
+                    var engine = new CakeEngine();
+                    engine.Task("A").Does(x => result.Add("A"));
+                    engine.Task("B").IsDependentOn("A").WithCriteria(false).Does(x => result.Add("B"));
+                    engine.Task("C").IsDependentOn("B").Does(x => result.Add("C"));
+
+                    // When
+                    engine.Run("C");
+
+                    // Then
+                    Assert.Equal(2, result.Count);
+                    Assert.Equal("A", result[0]);
+                    Assert.Equal("C", result[1]);
+                }
+            }
+        }
+    }
+}
