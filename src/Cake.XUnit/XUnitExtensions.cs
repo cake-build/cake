dd126650777e63b88d2883268e9b6829e2ed139e
Patrik Svensson <patrik@patriksvensson.se>
2014-06-03 14:45:47 +0200

    Added convenience overload for xUnit.

--- src/Cake.XUnit/XUnitExtensions.cs
+++ src/Cake.XUnit/XUnitExtensions.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections.Generic;
 using Cake.Core;
+using Cake.Core.Extensions;
 using Cake.Core.IO;
 
 namespace Cake.XUnit
 {
     public static class XUnitExtensions
     {
+        public static void XUnit(this ICakeContext context, string pattern)
+        {
+            var assemblies = context.GetFiles(pattern);
+            var settings = new XUnitSettings(assemblies);
+            new XUnitRunner().Run(context, settings);
+        }
+
         public static void XUnit(this ICakeContext context, IEnumerable<FilePath> assemblies)
         {
             var settings = new XUnitSettings(assemblies);
