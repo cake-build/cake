13531b3f05e63b330cf55207dcec34f96cf555a5
Patrik Svensson <patrik@patriksvensson.se>
2014-06-06 17:41:42 +0200

    Added script host methods for built in functionality.

---
+++ src/Cake/Scripting/Host/ScriptHost.XUnit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cake.Core;
+using Cake.Core.IO;
+using Cake.MSBuild;
+using Cake.XUnit;
+
+namespace Cake.Scripting.Host
+{
+    public sealed partial class ScriptHost
+    {
+        public void XUnit(string pattern)
+        {
+            ((ICakeContext)this).XUnit(pattern);
+        }
+
+        public void XUnit(IEnumerable<FilePath> assemblies)
+        {
+            ((ICakeContext)this).XUnit(assemblies);
+        }
+    }
+}
