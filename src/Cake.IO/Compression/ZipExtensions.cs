bf235c55d3405c6f602f80b80082c0f2b5591448
Patrik Svensson <patrik@patriksvensson.se>
2014-06-08 13:12:53 +0200

    Added zip convenience overload that takes a glob expression.

--- src/Cake.IO/Compression/ZipExtensions.cs
+++ src/Cake.IO/Compression/ZipExtensions.cs
@@ -6,6 +6,13 @@ namespace Cake.IO.Compression
 {
     public static class ZipExtensions
     {
+        public static void Zip(this ICakeContext context, DirectoryPath root, FilePath output, string pattern)
+        {
+            var zipper = new Zipper(context.FileSystem, context.Environment, context.Log);
+            var files = context.GetFiles(pattern);
+            zipper.Zip(root, output, files);
+        }
+
         public static void Zip(this ICakeContext context, DirectoryPath root, FilePath output, IEnumerable<FilePath> files)
         {
             var zipper = new Zipper(context.FileSystem, context.Environment, context.Log);
