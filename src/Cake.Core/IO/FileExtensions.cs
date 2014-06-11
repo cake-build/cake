af9a9cb505027942e9dd2b4754f4200ec361cddc
Patrik Svensson <patrik@patriksvensson.se>
2014-06-11 20:47:24 +0200

    Added file extensions.

---
+++ src/Cake.Core/IO/FileExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace Cake.Core.IO
+{
+    public static class FileExtensions
+    {
+        public static Stream Open(this IFile file, FileMode mode)
+        {
+            return file.Open(mode, 
+                mode == FileMode.Append ? FileAccess.Write : FileAccess.ReadWrite, 
+                FileShare.None);
+        }
+
+        public static Stream Open(this IFile file, FileMode mode, FileAccess access)
+        {
+            return file.Open(mode, access, FileShare.None);
+        }
+
+        public static Stream OpenRead(this IFile file)
+        {
+            return file.Open(FileMode.Open, FileAccess.Read, FileShare.Read);
+        }
+
+        public static Stream OpenWrite(this IFile file)
+        {
+            return file.Open(FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+        }
+    }
+}
