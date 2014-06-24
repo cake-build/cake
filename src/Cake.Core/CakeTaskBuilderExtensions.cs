f941da1461be420267a0a050f385f38bc9779f92
Mattias Karlsson <matkar@wcom.se>
2014-06-24 23:18:35 +0200

    Added support for custom error handling Added CakeTaskBuilderExtensions Does overloads for possibility to intercept and act on errors, with option to continue or fail on error.

--- src/Cake.Core/CakeTaskBuilderExtensions.cs
+++ src/Cake.Core/CakeTaskBuilderExtensions.cs
@@ -40,6 +40,76 @@ namespace Cake.Core
             return builder;
         }
 
+        
+        public static CakeTaskBuilder<ActionTask> Does(this CakeTaskBuilder<ActionTask> builder, Action action, Func<Exception, bool> errorAction )
+        {
+            if (errorAction == null)
+            {
+                throw new ArgumentNullException("errorAction");
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            return builder.Does(context=>action(), (context, exception)=>errorAction(exception));
+        }
+
+        public static CakeTaskBuilder<ActionTask> Does(this CakeTaskBuilder<ActionTask> builder, Action action, Func<ICakeContext, Exception, bool> errorAction )
+        {
+            if (errorAction == null)
+            {
+                throw new ArgumentNullException("errorAction");
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            return builder.Does(context=>action(), errorAction);
+        }
+
+        public static CakeTaskBuilder<ActionTask> Does(this CakeTaskBuilder<ActionTask> builder, Action<ICakeContext> action, Func<ICakeContext, Exception, bool> errorAction )
+        {
+            if (errorAction == null)
+            {
+                throw new ArgumentNullException("errorAction");
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            Action<ICakeContext> proxyAction = context => CreateProxyAction(action, errorAction, context);
+            return builder.Does(proxyAction);
+        }
+
+        private static void CreateProxyAction(Action<ICakeContext> action, Func<ICakeContext, Exception, bool> errorAction, ICakeContext context)
+        {
+            try
+            {
+                action(context);
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    if (!errorAction(context, ex))
+                        throw;
+                }
+                catch (Exception errorEx)
+                {
+                    throw new AggregateException(
+                        ex,
+                        errorEx
+                        );
+                }
+            }
+        }
+
         public static CakeTaskBuilder<ActionTask> ContinueOnError(this CakeTaskBuilder<ActionTask> builder)
         {
             builder.Task.ContinueOnError = true;
