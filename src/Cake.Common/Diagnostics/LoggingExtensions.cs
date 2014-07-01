524232c96f5a1ca297af07c211ee6f174912435c
Patrik Svensson <patrik@patriksvensson.se>
2014-07-01 03:21:51 +0200

    Added script methods for log.

---
+++ src/Cake.Common/Diagnostics/LoggingExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.Diagnostics;
+
+namespace Cake.Common.Diagnostics
+{
+    public static class LoggingExtensions
+    {
+        [CakeScriptMethod]
+        public static void Error(this ICakeContext context, string format, params object[] args)
+        {
+            context.Log.Error(format, args);
+        }
+
+        [CakeScriptMethod]
+        public static void Warning(this ICakeContext context, string format, params object[] args)
+        {
+            context.Log.Warning(format, args);
+        }
+
+        [CakeScriptMethod]
+        public static void Information(this ICakeContext context, string format, params object[] args)
+        {
+            context.Log.Information(format, args);
+        }
+
+        [CakeScriptMethod]
+        public static void Verbose(this ICakeContext context, string format, params object[] args)
+        {
+            context.Log.Verbose(format, args);
+        }
+
+        [CakeScriptMethod]
+        public static void Debug(this ICakeContext context, string format, params object[] args)
+        {
+            context.Log.Debug(format, args);
+        }
+    }
+}
