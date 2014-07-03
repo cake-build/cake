4b34d174c875d3359cc1094dcac3b65771546b6a
Viktor Elofsson <viktor@viktorelofsson.se>
2014-07-04 00:09:06 +0200

    Obvious bug is obvious.

--- src/Cake.Common/Tools/WiX/WiXExtensions.cs
+++ src/Cake.Common/Tools/WiX/WiXExtensions.cs
@@ -8,14 +8,14 @@ namespace Cake.Common.Tools.WiX
     public static class WiXExtensions
     {
         [CakeMethodAlias]
-        public static void WiXLight(this ICakeContext context, string pattern, CandleSettings settings = null)
+        public static void WiXCandle(this ICakeContext context, string pattern, CandleSettings settings = null)
         {
             var files = context.Globber.GetFiles(pattern);
-            WiXLight(context, files, settings ?? new CandleSettings());
+            WiXCandle(context, files, settings ?? new CandleSettings());
         }
 
         [CakeMethodAlias]
-        public static void WiXLight(this ICakeContext context, IEnumerable<FilePath> sourceFiles, CandleSettings settings = null)
+        public static void WiXCandle(this ICakeContext context, IEnumerable<FilePath> sourceFiles, CandleSettings settings = null)
         {
             var runner = new CandleRunner(context.Environment, context.Globber, context.ProcessRunner);
             runner.Run(sourceFiles, settings ?? new CandleSettings());
