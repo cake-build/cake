1ac8dfb158a4c0e4c6595f457a9473d2b112c2b9
Richard Simpson <richard@clear-measure.com>
2014-08-20 07:52:50 -0500

    Added GetEnvVar helper.

---
+++ src/Cake.Common/EnviromentExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+
+namespace Cake.Common
+{
+    /// <summary>
+    /// Added helpers to retrieve enviroment information
+    /// </summary>
+    [CakeAliasCategory("Environment")]
+    public static class EnvironmentExtensions
+    {
+        /// <summary>
+        /// Retrieves the value of the enviroment variable.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="variable">The environment variable.</param>
+        /// <returns></returns>
+        [CakeMethodAlias]
+        public static string GetEnvVar(this ICakeContext context, string variable)
+        {
+            return context.Environment.GetEnvironmentVariable(variable);
+        }
+    }
+}
