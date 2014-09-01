7540014ba53cfcf610aadf0b9b169b027c73d13c
Mattias Karlsson <matkar@wcom.se>
2014-09-01 13:19:50 +0200

    Added Sign extension for assembly certificate signing This adds support for PFX certificate signing (a.k.a Authenticode not to be confused with strong naming) using the standard Windows SDK SignTool Example usage: Task("Sign")     .IsDependentOn("Clean")     .IsDependentOn("Restore")     .IsDependentOn("Build")     .Does(() => {     var files = GetFiles(solutionDir + "/**/bin/" + configuration + "/**/*.exe");     Sign(files, new SignToolSignSettings {             TimeStampUri = new Uri("http://timestamp.digicert.com"),             CertPath = "digicert.pfx",             Password = "TopSecret"     }); });
    Cleaned up resolver and FilePath.Exists()
    
    Added category

---
+++ src/Cake.Common/Tools/SignTool/Sign/SignToolSignRunner.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Globalization;
+using Cake.Core;
+using Cake.Core.IO;
+using Cake.Core.Utilities;
+using File=System.IO.File;
+
+namespace Cake.Common.Tools.SignTool.Sign
+{
+    /// <summary>
+    /// The SignTool SIGN assembly runner
+    /// </summary>
+    public sealed class SignToolSignRunner : Tool<SignToolSignSettings>
+    {
+        private readonly ICakeEnvironment _environment;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SignToolSignRunner"/> class.
+        /// </summary>
+        /// <param name="fileSystem">The file system.</param>
+        /// <param name="environment">The environment.</param>
+        /// <param name="processRunner">The process runner.</param>
+        public SignToolSignRunner(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner processRunner) : base(fileSystem, environment, processRunner)
+        {
+            _environment = environment;
+        }
+
+        /// <summary>
+        /// Runs the tests in the specified assembly.
+        /// </summary>
+        /// <param name="assemblyPath">The assembly path.</param>
+        /// <param name="settings">The settings.</param>
+        public void Run(FilePath assemblyPath, SignToolSignSettings settings)
+        {
+            if (assemblyPath == null)
+            {
+                throw new ArgumentNullException("assemblyPath");
+            }
+            if (settings == null)
+            {
+                throw new ArgumentNullException("settings");
+            }
+
+            Run(settings, GetArguments(assemblyPath, settings), settings.ToolPath);
+        }
+
+        private ToolArgumentBuilder GetArguments(FilePath assemblyPath, SignToolSignSettings settings)
+        {
+            if (assemblyPath==null || !File.Exists(assemblyPath.FullPath))
+               throw new ArgumentException(
+                    string.Format(CultureInfo.InvariantCulture, "{0}: AssemblyPath not specified or missing ({1})", GetToolName(), assemblyPath));
+
+
+            if (settings.TimeStampUri==null)
+               throw new ArgumentException(
+                    string.Format(CultureInfo.InvariantCulture, "{0}: TimeStampUri Required but not specified.", GetToolName()));
+
+            
+            if (settings.CertPath==null || !File.Exists(settings.CertPath.FullPath))
+               throw new ArgumentException(
+                    string.Format(CultureInfo.InvariantCulture, "{0}: CertPath not specified or missing ({1})", GetToolName(), settings.CertPath));
+
+            
+            if (string.IsNullOrEmpty(settings.Password))
+               throw new ArgumentException(
+                    string.Format(CultureInfo.InvariantCulture, "{0}: Password Required but not specified.", GetToolName()));
+
+            var builder = new ToolArgumentBuilder();
+
+            //SIGN Command
+            builder.AppendText("SIGN");
+
+
+            //TimeStamp server
+            builder.AppendText("/t");
+            builder.AppendQuotedText(settings.TimeStampUri.AbsoluteUri);
+
+            
+            //Path to PFX Certificate
+            builder.AppendText("/f");
+            builder.AppendQuotedText(settings.CertPath.MakeAbsolute(_environment).FullPath);
+
+            
+            //PFX Password
+            builder.AppendText("/p");
+            builder.AppendQuotedText(settings.Password);
+
+            //Target Assembly to sign
+            builder.AppendQuotedText(assemblyPath.MakeAbsolute(_environment).FullPath);
+
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Get name of tool
+        /// </summary>
+        /// <returns></returns>
+        protected override string GetToolName()
+        {
+            return "SignTool SIGN";
+        }
+
+        /// <summary>
+        /// Get the default path to tool
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        protected override FilePath GetDefaultToolPath(SignToolSignSettings settings)
+        {
+            return (settings==null ? null :  settings.ToolPath)
+                ?? SignToolResolver.GetSignToolPath(_environment);
+        }
+    }
+}
---
+++ src/Cake.Common/Tools/SignTool/Sign/SignToolSignSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Cake.Core;
+using Cake.Core.IO;
+
+namespace Cake.Common.Tools.SignTool.Sign
+{
+    /// <summary>
+    /// Contains settings used by  <see cref="SignToolSignRunner"/>.
+    /// </summary>
+    public sealed class SignToolSignSettings
+    {
+        /// <summary>
+        /// Gets or sets the tool path.
+        /// </summary>
+        /// <value>
+        /// The tool path.
+        /// Defaults to path given by <see cref="SignToolResolver.GetSignToolPath(ICakeEnvironment)"/>
+        /// </value>
+        public FilePath ToolPath { get; set; }
+
+        /// <summary>
+        /// Specify the timestamp server's URL.
+        /// </summary>
+        public Uri TimeStampUri { get; set; }
+
+        /// <summary>
+        /// Path to PFX cerificate
+        /// </summary>
+        public FilePath CertPath { get; set; }
+
+        /// <summary>
+        /// PFX certificate password
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
