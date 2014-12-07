02b3a9a5b9daf5968ca5d57e4b37892c6da69abe
Patrik Svensson <patrik@patriksvensson.se>
2014-12-07 23:26:05 +0100

    Updated build script.

---
+++ build.ps1
@@ -0,0 +1,28 @@
+Param(
+    [string]$Script = "build.cake",
+    [string]$Target = "Default",
+    [string]$Configuration = "Release",
+    [string]$Verbosity = "Verbose"
+)
+
+$TOOLS_DIR = Join-Path $PSScriptRoot "tools"
+$NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
+$CAKE_EXE = Join-Path $TOOLS_DIR "Cake/Cake.exe"
+
+# Make sure NuGet exists where we expect it.
+if (!(Test-Path $NUGET_EXE)) {
+    Throw "Could not find NuGet.exe"
+}
+
+# Restore tools from NuGet.
+Start-Process $NUGET_EXE -ArgumentList "install -ExcludeVersion" -WorkingDirectory $TOOLS_DIR -Wait -NoNewWindow
+
+# Make sure that Cake has been installed.
+if (!(Test-Path $CAKE_EXE)) {
+    Throw "Could not find Cake.exe"
+}
+
+# Start Cake
+Write-Host
+Start-Process $CAKE_EXE -Wait -NoNewWindow -ArgumentList "$Script -target=$Target -configuration=$Configuration -verbosity=$Verbosity"
+Write-Host
