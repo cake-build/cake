cbd44d0db5660f871cd7fb78df41bb1d47ac9197
Patrik Svensson <patrik@patriksvensson.se>
2015-02-10 20:04:07 +0100

    Added StyleCop to Cake.Testing.

--- src/Cake.Testing/Fakes/FakeFileStream.cs
+++ src/Cake.Testing/Fakes/FakeFileStream.cs
@@ -3,36 +3,58 @@ using System.IO;
 
 namespace Cake.Testing.Fakes
 {
+    /// <summary>
+    /// Implementation of a fake <see cref="Stream"/> used by <see cref="FakeFileSystem"/>.
+    /// </summary>
     public sealed class FakeFileStream : Stream
     {
         private readonly FakeFile _file;
         private long _position;
 
+        /// <summary>
+        /// When overridden in a derived class, gets a value indicating whether the current stream supports reading.
+        /// </summary>
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, gets a value indicating whether the current stream supports seeking.
+        /// </summary>
+        public override bool CanSeek
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, gets a value indicating whether the current stream supports writing.
+        /// </summary>
+        public override bool CanWrite
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeFileStream"/> class.
+        /// </summary>
+        /// <param name="file">The file.</param>
         public FakeFileStream(FakeFile file)
         {
             _file = file;
             _position = 0;
         }
 
-        public override bool CanRead
-        {
-            get { return true; }
-        }
-
-        public override bool CanSeek
-        {
-            get { return true; }
-        }
-
-        public override bool CanWrite
-        {
-            get { return true; }
-        }
-
+        /// <summary>
+        /// When overridden in a derived class, clears all buffers for this stream and causes any buffered data to be written to the underlying device.
+        /// </summary>
         public override void Flush()
         {
         }
 
+        /// <summary>
+        /// When overridden in a derived class, gets the length in bytes of the stream.
+        /// </summary>
         public override long Length
         {
             get
@@ -44,12 +66,24 @@ namespace Cake.Testing.Fakes
             }
         }
 
+        /// <summary>
+        /// When overridden in a derived class, gets or sets the position within the current stream.
+        /// </summary>
         public override long Position
         {
             get { return _position; }
             set { Seek(value, SeekOrigin.Begin); }
         }
 
+        /// <summary>
+        /// When overridden in a derived class, reads a sequence of bytes from the current stream and advances the position within the stream by the number of bytes read.
+        /// </summary>
+        /// <param name="buffer">An array of bytes. When this method returns, the buffer contains the specified byte array with the values between <paramref name="offset" /> and (<paramref name="offset" /> + <paramref name="count" /> - 1) replaced by the bytes read from the current source.</param>
+        /// <param name="offset">The zero-based byte offset in <paramref name="buffer" /> at which to begin storing the data read from the current stream.</param>
+        /// <param name="count">The maximum number of bytes to be read from the current stream.</param>
+        /// <returns>
+        /// The total number of bytes read into the buffer. This can be less than the number of bytes requested if that many bytes are not currently available, or zero (0) if the end of the stream has been reached.
+        /// </returns>
         public override int Read(byte[] buffer, int offset, int count)
         {
             lock (_file.ContentLock)
@@ -57,12 +91,20 @@ namespace Cake.Testing.Fakes
                 var end = _position + count;
                 var fileSize = _file.ContentLength;
                 var maxLengthToRead = end > fileSize ? fileSize - _position : count;
-                Buffer.BlockCopy(_file.Content, (int) _position, buffer, offset, (int) maxLengthToRead);
+                Buffer.BlockCopy(_file.Content, (int)_position, buffer, offset, (int)maxLengthToRead);
                 _position += maxLengthToRead;
-                return (int) maxLengthToRead;
+                return (int)maxLengthToRead;
             }
         }
 
+        /// <summary>
+        /// When overridden in a derived class, sets the position within the current stream.
+        /// </summary>
+        /// <param name="offset">A byte offset relative to the <paramref name="origin" /> parameter.</param>
+        /// <param name="origin">A value of type <see cref="T:System.IO.SeekOrigin" /> indicating the reference point used to obtain the new position.</param>
+        /// <returns>
+        /// The new position within the current stream.
+        /// </returns>
         public override long Seek(long offset, SeekOrigin origin)
         {
             if (origin == SeekOrigin.Begin)
@@ -80,6 +122,10 @@ namespace Cake.Testing.Fakes
             throw new NotSupportedException();
         }
 
+        /// <summary>
+        /// When overridden in a derived class, sets the length of the current stream.
+        /// </summary>
+        /// <param name="value">The desired length of the current stream in bytes.</param>
         public override void SetLength(long value)
         {
             lock (_file.ContentLock)
@@ -88,17 +134,23 @@ namespace Cake.Testing.Fakes
             }
         }
 
+        /// <summary>
+        /// When overridden in a derived class, writes a sequence of bytes to the current stream and advances the current position within this stream by the number of bytes written.
+        /// </summary>
+        /// <param name="buffer">An array of bytes. This method copies <paramref name="count" /> bytes from <paramref name="buffer" /> to the current stream.</param>
+        /// <param name="offset">The zero-based byte offset in <paramref name="buffer" /> at which to begin copying bytes to the current stream.</param>
+        /// <param name="count">The number of bytes to be written to the current stream.</param>
         public override void Write(byte[] buffer, int offset, int count)
         {
             lock (_file.ContentLock)
             {
                 var fileSize = _file.ContentLength;
-                var endOfWrite = (_position + count);
+                var endOfWrite = _position + count;
                 if (endOfWrite > fileSize)
                 {
                     _file.Resize(endOfWrite);
                 }
-                Buffer.BlockCopy(buffer, offset, _file.Content, (int) _position, count);
+                Buffer.BlockCopy(buffer, offset, _file.Content, (int)_position, count);
                 _position = _position + count;
             }
         }
--- src/Cake.Testing/Fakes/FakeFileSystem.cs
+++ src/Cake.Testing/Fakes/FakeFileSystem.cs
@@ -5,53 +5,98 @@ using Cake.Core.IO;
 
 namespace Cake.Testing.Fakes
 {
+    /// <summary>
+    /// Implementation of a fake <see cref="IFileSystem"/>.
+    /// </summary>
     public sealed class FakeFileSystem : IFileSystem
     {
         private readonly Dictionary<DirectoryPath, FakeDirectory> _directories;
         private readonly Dictionary<FilePath, FakeFile> _files;
 
+        /// <summary>
+        /// Gets the directories.
+        /// </summary>
+        /// <value>The directories.</value>
         public Dictionary<DirectoryPath, FakeDirectory> Directories
         {
             get { return _directories; }
         }
 
+        /// <summary>
+        /// Gets the files.
+        /// </summary>
+        /// <value>The files.</value>
         public Dictionary<FilePath, FakeFile> Files
         {
             get { return _files; }
         }
 
-        public FakeFileSystem(bool isUnix)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeFileSystem"/> class.
+        /// </summary>
+        /// <param name="isCaseSensitive">If set to <c>true</c> the file system is case sensitive.</param>
+        public FakeFileSystem(bool isCaseSensitive)
         {
-            _directories = new Dictionary<DirectoryPath, FakeDirectory>(new PathComparer(isUnix));
-            _files = new Dictionary<FilePath, FakeFile>(new PathComparer(isUnix));
+            _directories = new Dictionary<DirectoryPath, FakeDirectory>(new PathComparer(isCaseSensitive));
+            _files = new Dictionary<FilePath, FakeFile>(new PathComparer(isCaseSensitive));
         }
 
+        /// <summary>
+        /// Gets a <see cref="IFile" /> instance representing the specified path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>
+        /// A <see cref="IFile" /> instance representing the specified path.
+        /// </returns>
         public IFile GetFile(FilePath path)
         {
             return GetFile(path, false);
         }
 
+        /// <summary>
+        /// Gets the file.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="hidden">if set to <c>true</c> [hidden].</param>
+        /// <returns>A <see cref="IFile"/> instance.</returns>
         public IFile GetFile(FilePath path, bool hidden)
         {
             if (!Files.ContainsKey(path))
             {
-                Files.Add(path, new FakeFile(this, path){Hidden = hidden});
+                Files.Add(path, new FakeFile(this, path) { Hidden = hidden });
             }
             return Files[path];
         }
 
+        /// <summary>
+        /// Gets the created file.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>A <see cref="IFile"/> instance.</returns>
         public IFile GetCreatedFile(FilePath path)
         {
             return GetCreatedFile(path, false);
         }
 
+        /// <summary>
+        /// Gets the created file.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="hidden">if set to <c>true</c> [hidden].</param>
+        /// <returns>A <see cref="IFile"/> instance.</returns>
         public IFile GetCreatedFile(FilePath path, bool hidden)
         {
-            var file = GetFile(path,hidden);
+            var file = GetFile(path, hidden);
             file.Open(FileMode.Create, FileAccess.Write, FileShare.None).Close();
             return file;
         }
 
+        /// <summary>
+        /// Gets the created file.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="content">The content.</param>
+        /// <returns>A <see cref="IFile"/> instance.</returns>
         public IFile GetCreatedFile(FilePath path, string content)
         {
             var file = GetFile(path);
@@ -63,23 +108,48 @@ namespace Cake.Testing.Fakes
             return file;
         }
 
+        /// <summary>
+        /// Gets a <see cref="IDirectory" /> instance representing the specified path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>
+        /// A <see cref="IDirectory" /> instance representing the specified path.
+        /// </returns>
         public IDirectory GetDirectory(DirectoryPath path)
         {
-            return GetDirectory(path, creatable: true, hidden:false);
+            return GetDirectory(path, creatable: true, hidden: false);
         }
 
+        /// <summary>
+        /// Gets the created directory.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>A <see cref="IDirectory"/> instance.</returns>
         public IDirectory GetCreatedDirectory(DirectoryPath path)
         {
             return GetCreatedDirectory(path, false);
         }
 
+        /// <summary>
+        /// Gets the created directory.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="hidden">if set to <c>true</c> [hidden].</param>
+        /// <returns>A <see cref="IDirectory"/> instance.</returns>
         public IDirectory GetCreatedDirectory(DirectoryPath path, bool hidden)
         {
-            var directory = GetDirectory(path, creatable: true, hidden:hidden);
+            var directory = GetDirectory(path, creatable: true, hidden: hidden);
             directory.Create();
             return directory;
         }
 
+        /// <summary>
+        /// Gets the directory.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="creatable">if set to <c>true</c> [creatable].</param>
+        /// <param name="hidden">if set to <c>true</c> [hidden].</param>
+        /// <returns>A <see cref="IDirectory"/> instance.</returns>
         private IDirectory GetDirectory(DirectoryPath path, bool creatable, bool hidden)
         {
             if (!Directories.ContainsKey(path))
@@ -89,6 +159,11 @@ namespace Cake.Testing.Fakes
             return Directories[path];
         }
 
+        /// <summary>
+        /// Gets the content of the text.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>The text content of the file.</returns>
         public string GetTextContent(FilePath path)
         {
             var file = GetFile(path) as FakeFile;
