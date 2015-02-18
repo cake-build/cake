aaea5c8f390e3dc5a9b871c4ffd03937ae6c0f54
Mattias Karlsson <matkar@wcom.se>
2015-02-19 00:38:15 +0100

    Added aliases for executing cake scripts out of process

---
+++ src/Cake.Common/Tools/Cake/CakeAliases.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+
+namespace Cake.Common.Tools.Cake
+{
+    /// <summary>
+    /// Contains functionality related to running Cake scripts out of process.
+    /// </summary>
+    public static class CakeAliases
+    {
+        /// <summary>
+        /// Executes cake script out of process
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="cakeScriptPath">The script file.</param>
+        [CakeMethodAlias]
+        public static void CakeExecuteScript(this ICakeContext context, FilePath cakeScriptPath)
+        {
+            context.CakeExecuteScript(cakeScriptPath, null);
+        }
+
+        /// <summary>
+        /// Executes cake script out of process
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="cakeScriptPath">The script file.</param>
+        /// <param name="settings">The settings <see cref="CakeSettings"/>.</param>
+        [CakeMethodAlias]
+        public static void CakeExecuteScript(this ICakeContext context, FilePath cakeScriptPath, CakeSettings settings)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var cakeRunner = new CakeRunner(context.FileSystem, context.Environment, context.Globber, context.ProcessRunner);
+            cakeRunner.ExecuteScript(cakeScriptPath, settings);
+        }
+
+        /// <summary>
+        /// Executes Cake expression out of process
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="cakeExpression">The cake expression</param>
+        [CakeMethodAlias]
+        public static void CakeExecuteExpression(this ICakeContext context, string cakeExpression)
+        {
+            context.CakeExecuteExpression(cakeExpression, null);
+        }
+
+        /// <summary>
+        /// Executes Cake expression out of process
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="cakeExpression">The cake expression</param>
+        /// <param name="settings">The settings <see cref="CakeSettings"/>.</param>
+        [CakeMethodAlias]
+        public static void CakeExecuteExpression(this ICakeContext context, string cakeExpression, CakeSettings settings)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var cakeRunner = new CakeRunner(context.FileSystem, context.Environment, context.Globber, context.ProcessRunner);
+            cakeRunner.ExecuteExpression(cakeExpression, settings);
+        }
+    }
+}
---
+++ src/Cake.Common/Tools/Cake/CakeRunner.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Cake.Core;
+using Cake.Core.IO;
+using Cake.Core.Utilities;
+
+namespace Cake.Common.Tools.Cake
+{
+    /// <summary>
+    /// Cake out process runner
+    /// </summary>
+    public sealed class CakeRunner : Tool<CakeSettings>
+    {
+        private readonly ICakeEnvironment _environment;
+        private readonly IGlobber _globber;
+        private readonly IFileSystem _fileSystem;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CakeRunner"/> class.
+        /// </summary>
+        /// <param name="fileSystem">The file system.</param>
+        /// <param name="environment">The environment.</param>
+        /// <param name="globber">The globber.</param>
+        /// <param name="processRunner">The process runner.</param>
+        public CakeRunner(IFileSystem fileSystem, ICakeEnvironment environment, IGlobber globber,
+            IProcessRunner processRunner)
+            : base(fileSystem, environment, processRunner)
+        {
+            _environment = environment;
+            _globber = globber;
+            _fileSystem = fileSystem;
+        }
+
+        /// <summary>
+        /// Executes supplied cake script in own process and supplied settings
+        /// </summary>
+        /// <param name="scriptPath">Path to script to execute</param>
+        /// <param name="settings">optional cake settings</param>
+        public void ExecuteScript(FilePath scriptPath, CakeSettings settings = null)
+        {
+            if (scriptPath == null)
+            {
+                throw new ArgumentNullException("scriptPath");
+            }
+
+            if (!_fileSystem.GetFile(scriptPath).Exists)
+            {
+                throw new FileNotFoundException("Cake script file not found.", scriptPath.FullPath);
+            }
+
+            settings = settings ?? new CakeSettings();
+
+            Run(settings, GetArguments(scriptPath, settings), settings.ToolPath);
+        }
+
+        /// <summary>
+        /// Executes supplied cake code expression in own process and supplied settings
+        /// </summary>
+        /// <param name="cakeExpression">Code expression to execute</param>
+        /// <param name="settings">optional cake settings</param>
+       [SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times")]
+       public void ExecuteExpression(string cakeExpression, CakeSettings settings = null)
+        {
+            if (string.IsNullOrWhiteSpace(cakeExpression))
+            {
+                throw new ArgumentNullException("cakeExpression");
+            }
+            DirectoryPath tempPath = _environment.GetEnvironmentVariable("TEMP") ?? "./";
+            var tempScriptFile = _fileSystem.GetFile(tempPath
+                .CombineWithFilePath(string.Format(CultureInfo.InvariantCulture, "{0}.cake", Guid.NewGuid()))
+                .MakeAbsolute(_environment));
+            try
+            {
+                using (var stream = tempScriptFile.OpenWrite())
+                {
+                    using (var streamWriter = new StreamWriter(stream, Encoding.UTF8))
+                    {
+                        streamWriter.WriteLine(cakeExpression);
+                    }
+                }
+                ExecuteScript(tempScriptFile.Path.FullPath, settings);
+            }
+            finally
+            {
+                if (tempScriptFile.Exists)
+                {
+                    tempScriptFile.Delete();
+                }
+            }
+        }
+
+        private ProcessArgumentBuilder GetArguments(FilePath scriptPath, CakeSettings settings)
+        {
+            var builder = new ProcessArgumentBuilder();
+            builder.AppendQuoted(scriptPath.MakeAbsolute(_environment).FullPath);
+
+            if (settings.Verbosity.HasValue)
+            {
+                builder.Append(string.Concat("-verbosity=", settings.Verbosity.Value.ToString()));
+            }
+
+            if (settings.Arguments != null)
+            {
+                foreach (var argument in settings.Arguments)
+                {
+                    builder.Append(string.Format(
+                        CultureInfo.InvariantCulture,
+                        "-{0}={1}",
+                        argument.Key,
+                        (argument.Value ?? string.Empty).Quote()));
+                }
+            }
+            return builder;
+        }
+
+        /// <summary>
+        /// Gets the name of the tool.
+        /// </summary>
+        /// <returns>The name of the tool.</returns>
+        protected override string GetToolName()
+        {
+            return "Cake";
+        }
+
+        /// <summary>
+        /// Gets the default tool path.
+        /// </summary>
+        /// <param name="settings">The settings.</param>
+        /// <returns>The default tool path.</returns>
+        protected override FilePath GetDefaultToolPath(CakeSettings settings)
+        {
+            const string expression = "./tools/**/Cake.exe";
+            return _globber.GetFiles(expression).FirstOrDefault();
+        }
+    }
+}
