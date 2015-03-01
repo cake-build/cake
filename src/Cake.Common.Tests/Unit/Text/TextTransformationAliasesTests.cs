f72983694f3172f0fcc834fdc5f57bb78a3633de
Patrik Svensson <patrik@patriksvensson.se>
2015-03-01 20:18:11 +0100

    Made text transformation placeholders configurable.

--- src/Cake.Common.Tests/Unit/Text/TextTransformationAliasesTests.cs
+++ src/Cake.Common.Tests/Unit/Text/TextTransformationAliasesTests.cs
@@ -11,94 +11,330 @@ namespace Cake.Common.Tests.Unit.Text
     {
         public sealed class TheTransformTextMethod
         {
-            [Fact]
-            public void Should_Throw_If_Context_Is_Null()
+            public sealed class WithDefaultPlaceholder
             {
-                // Given, When
-                var result = Record.Exception(() => 
-                    TextTransformationAliases.TransformText(null, "Hello World"));
+                [Fact]
+                public void Should_Throw_If_Context_Is_Null()
+                {
+                    // Given, When
+                    var result = Record.Exception(() =>
+                        TextTransformationAliases.TransformText(null, "Hello World"));
 
-                // Then
-                Assert.IsArgumentNullException(result, "context");
+                    // Then
+                    Assert.IsArgumentNullException(result, "context");
+                }
+
+                [Fact]
+                public void Should_Throw_If_Template_Is_Null()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+
+                    // When
+                    var result = Record.Exception(() =>
+                        TextTransformationAliases.TransformText(context, null));
+
+                    // Then
+                    Assert.IsArgumentNullException(result, "template");
+                }
+
+                [Fact]
+                public void Should_Create_Text_Transformation()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+
+                    // When
+                    var result = TextTransformationAliases.TransformText(context, "Hello World");
+
+                    // Then
+                    Assert.Equal("Hello World", result.ToString());
+                }
+
+                [Fact]
+                public void Should_Transform_Text_Using_Specified_Placeholders()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+                    var transform = TextTransformationAliases.TransformText(context, "Hello <%subject%>");
+                    transform.WithToken("subject", "World");
+
+                    // When
+                    var result = transform.ToString();
+
+                    // Then
+                    Assert.Equal("Hello World", result);
+                }
             }
 
-            [Fact]
-            public void Should_Throw_If_Template_Is_Null()
+            public sealed class WithCustomPlaceholder
             {
-                // Given
-                var context = Substitute.For<ICakeContext>();
+                [Fact]
+                public void Should_Throw_If_Context_Is_Null()
+                {
+                    // Given, When
+                    var result = Record.Exception(() =>
+                        TextTransformationAliases.TransformText(null, "Hello World", "{", "}"));
 
-                // When
-                var result = Record.Exception(() =>
-                    TextTransformationAliases.TransformText(context, null));
+                    // Then
+                    Assert.IsArgumentNullException(result, "context");
+                }
 
-                // Then
-                Assert.IsArgumentNullException(result, "template");
+                [Fact]
+                public void Should_Throw_If_Template_Is_Null()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+
+                    // When
+                    var result = Record.Exception(() =>
+                        TextTransformationAliases.TransformText(context, null, "{", "}"));
+
+                    // Then
+                    Assert.IsArgumentNullException(result, "template");
+                }
+
+                [Fact]
+                public void Should_Throw_If_Left_Placeholder_Is_Null()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+
+                    // When
+                    var result = Record.Exception(() =>
+                        TextTransformationAliases.TransformText(context, null, null, "}"));
+
+                    // Then
+                    Assert.IsArgumentNullException(result, "leftPlaceholder");
+                }
+
+                [Fact]
+                public void Should_Throw_If_Right_Placeholder_Is_Null()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+
+                    // When
+                    var result = Record.Exception(() =>
+                        TextTransformationAliases.TransformText(context, null, "{", null));
+
+                    // Then
+                    Assert.IsArgumentNullException(result, "rightPlaceholder");
+                }
+
+                [Fact]
+                public void Should_Create_Text_Transformation()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+
+                    // When
+                    var result = TextTransformationAliases.TransformText(context, "Hello World", "{", "}");
+
+                    // Then
+                    Assert.Equal("Hello World", result.ToString());
+                }
+
+                [Fact]
+                public void Should_Transform_Text_Using_Specified_Placeholders()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+                    var transform = TextTransformationAliases.TransformText(context, "Hello {subject}", "{", "}");
+                    transform.WithToken("subject", "World");
+
+                    // When
+                    var result = transform.ToString();
+
+                    // Then
+                    Assert.Equal("Hello World", result);
+                }
             }
-
-            [Fact]
-            public void Should_Create_Text_Transformation()
-            {
-                // Given
-                var context = Substitute.For<ICakeContext>();
-               
-                // When
-                var result = TextTransformationAliases.TransformText(context, "Hello World");
-
-                // Then
-                Assert.Equal("Hello World", result.ToString());
-           }
         }
 
         public sealed class TheTransformTextFileMethod
         {
-            [Fact]
-            public void Should_Throw_If_Context_Is_Null()
+            public sealed class WithDefaultPlaceholder
             {
-                // Given, When
-                var result = Record.Exception(() =>
-                    TextTransformationAliases.TransformTextFile(
-                        null, new FilePath("./template.txt")));
+                [Fact]
+                public void Should_Throw_If_Context_Is_Null()
+                {
+                    // Given, When
+                    var result = Record.Exception(() =>
+                        TextTransformationAliases.TransformTextFile(
+                            null, new FilePath("./template.txt")));
 
-                // Then
-                Assert.IsArgumentNullException(result, "context");
+                    // Then
+                    Assert.IsArgumentNullException(result, "context");
+                }
+
+                [Fact]
+                public void Should_Throw_If_Template_Is_Null()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+
+                    // When
+                    var result = Record.Exception(() =>
+                        TextTransformationAliases.TransformTextFile(
+                            context, null));
+
+                    // Then
+                    Assert.IsArgumentNullException(result, "path");
+                }
+
+                [Fact]
+                public void Should_Create_Text_Transformation_From_Disc_Template()
+                {
+                    // Given
+                    var fileSystem = new FakeFileSystem(false);
+                    fileSystem.GetCreatedFile("/Working/template.txt", "Hello World");
+
+                    var environment = Substitute.For<ICakeEnvironment>();
+                    environment.WorkingDirectory.Returns("/Working");
+
+                    var context = Substitute.For<ICakeContext>();
+                    context.FileSystem.Returns(fileSystem);
+                    context.Environment.Returns(environment);
+
+                    // When
+                    var result = TextTransformationAliases.TransformTextFile(
+                        context, "./template.txt");
+
+                    // Then
+                    Assert.Equal("Hello World", result.ToString());
+                }
+
+                [Fact]
+                public void Should_Transform_Text_From_Disc_Template_Using_Default_Placeholders()
+                {
+                    // Given
+                    var fileSystem = new FakeFileSystem(false);
+                    fileSystem.GetCreatedFile("/Working/template.txt", "Hello <%subject%>");
+
+                    var environment = Substitute.For<ICakeEnvironment>();
+                    environment.WorkingDirectory.Returns("/Working");
+
+                    var context = Substitute.For<ICakeContext>();
+                    context.FileSystem.Returns(fileSystem);
+                    context.Environment.Returns(environment);
+
+                    var transform = TextTransformationAliases.TransformTextFile(context, "./template.txt");
+                    transform.WithToken("subject", "World");
+
+                    // When
+                    var result = transform.ToString();
+
+                    // Then
+                    Assert.Equal("Hello World", result);
+                }
             }
 
-            [Fact]
-            public void Should_Throw_If_Template_Is_Null()
+            public sealed class WithCustomPlaceholder
             {
-                // Given
-                var context = Substitute.For<ICakeContext>();
+                [Fact]
+                public void Should_Throw_If_Context_Is_Null()
+                {
+                    // Given, When
+                    var result = Record.Exception(() =>
+                        TextTransformationAliases.TransformTextFile(
+                            null, new FilePath("./template.txt"), "{", "}"));
 
-                // When
-                var result = Record.Exception(() =>
-                    TextTransformationAliases.TransformTextFile(
-                        context, null));
+                    // Then
+                    Assert.IsArgumentNullException(result, "context");
+                }
 
-                // Then
-                Assert.IsArgumentNullException(result, "path");
-            }
+                [Fact]
+                public void Should_Throw_If_Template_Is_Null()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
 
-            [Fact]
-            public void Should_Create_Text_Transformation_From_Disc_Template()
-            {
-                // Given
-                var fileSystem = new FakeFileSystem(false);
-                fileSystem.GetCreatedFile("/Working/template.txt", "Hello World");
-                
-                var environment = Substitute.For<ICakeEnvironment>();
-                environment.WorkingDirectory.Returns("/Working");
+                    // When
+                    var result = Record.Exception(() =>
+                        TextTransformationAliases.TransformTextFile(
+                            context, null, "{", "}"));
 
-                var context = Substitute.For<ICakeContext>();
-                context.FileSystem.Returns(fileSystem);
-                context.Environment.Returns(environment);
+                    // Then
+                    Assert.IsArgumentNullException(result, "path");
+                }
 
-                // When
-                var result = TextTransformationAliases.TransformTextFile(
-                    context, "./template.txt");
+                [Fact]
+                public void Should_Throw_If_Left_Placeholder_Is_Null()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
 
-                // Then
-                Assert.Equal("Hello World", result.ToString());
+                    // When
+                    var result = Record.Exception(() =>
+                        TextTransformationAliases.TransformTextFile(
+                            context, new FilePath("./template.txt"), null, "}"));
+
+                    // Then
+                    Assert.IsArgumentNullException(result, "leftPlaceholder");
+                }
+
+                [Fact]
+                public void Should_Throw_If_Right_Placeholder_Is_Null()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+
+                    // When
+                    var result = Record.Exception(() =>
+                        TextTransformationAliases.TransformTextFile(
+                            context, new FilePath("./template.txt"), "{", null));
+
+                    // Then
+                    Assert.IsArgumentNullException(result, "rightPlaceholder");
+                }
+
+                [Fact]
+                public void Should_Create_Text_Transformation_From_Disc_Template()
+                {
+                    // Given
+                    var fileSystem = new FakeFileSystem(false);
+                    fileSystem.GetCreatedFile("/Working/template.txt", "Hello World");
+
+                    var environment = Substitute.For<ICakeEnvironment>();
+                    environment.WorkingDirectory.Returns("/Working");
+
+                    var context = Substitute.For<ICakeContext>();
+                    context.FileSystem.Returns(fileSystem);
+                    context.Environment.Returns(environment);
+
+                    // When
+                    var result = TextTransformationAliases.TransformTextFile(
+                        context, "./template.txt", "{", "}");
+
+                    // Then
+                    Assert.Equal("Hello World", result.ToString());
+                }
+
+                [Fact]
+                public void Should_Transform_Text_From_Disc_Template_Using_Specified_Placeholders()
+                {
+                    // Given
+                    var fileSystem = new FakeFileSystem(false);
+                    fileSystem.GetCreatedFile("/Working/template.txt", "Hello {subject}");
+
+                    var environment = Substitute.For<ICakeEnvironment>();
+                    environment.WorkingDirectory.Returns("/Working");
+
+                    var context = Substitute.For<ICakeContext>();
+                    context.FileSystem.Returns(fileSystem);
+                    context.Environment.Returns(environment);
+
+                    var transform = TextTransformationAliases.TransformTextFile(context, "./template.txt", "{", "}");
+                    transform.WithToken("subject", "World");
+
+                    // When
+                    var result = transform.ToString();
+
+
+                    // Then
+                    Assert.Equal("Hello World", result);
+                }
             }
         }
     }
