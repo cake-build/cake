b0304eb2ee6c26b7d50a52ddde0487f4f1b1ea15
Phillip Sharpe <code@sharperad.com>
2015-05-22 13:29:17 +0100

    Argument ToString Methods
    Added missing ToString methods

--- src/Cake.Core/IO/Arguments/QuotedArgument.cs
+++ src/Cake.Core/IO/Arguments/QuotedArgument.cs
@@ -35,5 +35,16 @@
         {
             return string.Concat("\"", _argument.RenderSafe(), "\"");
         }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return this.RenderSafe();
+        }
     }
 }
--- src/Cake.Core/IO/Arguments/TextArgument.cs
+++ src/Cake.Core/IO/Arguments/TextArgument.cs
@@ -39,5 +39,16 @@
         {
             return Render();
         }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return this.RenderSafe();
+        }
     }
 }
