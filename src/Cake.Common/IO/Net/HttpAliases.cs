8a8392bcdffa448685a080c0ef197f8b19f940cb
Redth <jondick@gmail.com>
2015-07-02 11:19:24 -0400

    Wrapped WebClient in using statement

--- src/Cake.Common/IO/Net/HttpAliases.cs
+++ src/Cake.Common/IO/Net/HttpAliases.cs
@@ -39,19 +39,21 @@ namespace Cake.Common
             // We track the last posted value since the event seems to fire many times for the same value
             var percentComplete = 0;
 
-            var http = new System.Net.WebClient();
-            http.DownloadProgressChanged += (sender, e) => 
+            using (var http = new System.Net.WebClient()) 
             {
-                // Only write to log if the value changed and only ever 5%
-                if (percentComplete != e.ProgressPercentage && e.ProgressPercentage % 5 == 0)
+                http.DownloadProgressChanged += (sender, e) => 
                 {
-                    percentComplete = e.ProgressPercentage;
-                    context.Log.Write(Verbosity.Verbose, LogLevel.Information, "Downloading File: {0}%", e.ProgressPercentage);
-                }
-            };
+                    // Only write to log if the value changed and only ever 5%
+                    if (percentComplete != e.ProgressPercentage && e.ProgressPercentage % 5 == 0) 
+                    {
+                        percentComplete = e.ProgressPercentage;
+                        context.Log.Write(Verbosity.Verbose, LogLevel.Information, "Downloading File: {0}%", e.ProgressPercentage);
+                    }
+                };
 
-            // Download file async so we get the progress events, but block for it to complete anyway
-            http.DownloadFileTaskAsync(url, outputPath.FullPath).Wait();
+                // Download file async so we get the progress events, but block for it to complete anyway
+                http.DownloadFileTaskAsync(url, outputPath.FullPath).Wait();
+            }
 
             context.Log.Write(Verbosity.Verbose, LogLevel.Information, "Download Complete, saved to: {0}", outputPath.FullPath);
         }
