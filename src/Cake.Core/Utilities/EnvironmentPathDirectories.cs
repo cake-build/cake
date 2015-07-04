cc0a0577e8c0685dad0e3a070d1c0ae4c5c49cb0
Redth <jondick@gmail.com>
2015-07-04 11:50:40 -0400

    Fixed visibility of helper method and added documentation for the class/method

--- src/Cake.Core/Utilities/EnvironmentPathDirectories.cs
+++ src/Cake.Core/Utilities/EnvironmentPathDirectories.cs
@@ -3,8 +3,18 @@ using Cake.Core.IO;
 
 namespace Cake.Core.Utilities
 {
-    internal static class EnvironmentPathDirectories
+    /// <summary>
+    /// Contains utilities to search for files in environment path directories.
+    /// </summary>
+    public static class EnvironmentPathDirectories
     {
+        /// <summary>
+        /// Finds the specified file in one of the directories listed in the PATH environment variable
+        /// </summary>
+        /// <returns>The file path, or null if no file was found.</returns>
+        /// <param name="fileSystem">The file system.</param>
+        /// <param name="environment">The environment.</param>
+        /// <param name="fileName">The file name to find.</param>
         public static FilePath FindFile(IFileSystem fileSystem, ICakeEnvironment environment, FilePath fileName)
         {            
             if (fileSystem.Exist(fileName))
