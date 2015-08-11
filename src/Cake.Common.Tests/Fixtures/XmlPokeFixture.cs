eb4e97e87a9e9439638eeb1b74d501c9bb936925
Richard Simpson <richardsimpson@outlook.com>
2015-08-11 18:17:28 -0500

    Added tests around XmlPoke and fix a bug with file write.

---
+++ src/Cake.Common.Tests/Fixtures/XmlPokeFixture.cs
@@ -0,0 +1,101 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml;
+
+using Cake.Common.Tests.Properties;
+using Cake.Common.Xml;
+using Cake.Core.IO;
+using Cake.Testing.Fakes;
+
+namespace Cake.Common.Tests.Fixtures
+{
+    using Cake.Core;
+
+    using NSubstitute;
+
+    internal sealed class XmlPokeFixture
+    {
+       public IFileSystem FileSystem { get; set; }
+       public ICakeContext Context { get; set; }
+       public FilePath XmlPath { get; set; }
+       public XmlPokeSettings Settings { get; set; }
+
+       public XmlPokeFixture(bool xmlExists = true)
+       {
+           Settings = new XmlPokeSettings();
+
+           var environment = FakeEnvironment.CreateUnixEnvironment();
+           var fileSystem = new FakeFileSystem(environment);
+           fileSystem.CreateDirectory("/Working");
+
+           if (xmlExists)
+           {
+               var xmlFile = fileSystem.CreateFile("/Working/web.config").SetContent(Resources.XmlPoke_Xml);
+               XmlPath = xmlFile.Path;
+           }
+
+           FileSystem = fileSystem;
+
+           Context = Substitute.For<ICakeContext>();
+           Context.FileSystem.Returns(FileSystem);
+           Context.Environment.Returns(environment);
+       }
+
+       public void Poke(string xpath, string value)
+       {
+           XmlPokeAliases.XmlPoke(Context, XmlPath, xpath, value, Settings);
+       }
+
+        public bool TestIsValue(string xpath, string value)
+        {
+            var xmlString = new StreamReader(FileSystem.GetFile(XmlPath).OpenRead()).ReadToEnd();
+            return TestIsValue(xmlString, xpath, value);
+        }
+
+        public bool TestIsValue(string xml, string xpath, string value)
+        {
+            using (var reader = new StringReader(xml))
+            using (var xmlReader = XmlReader.Create(reader))
+            {
+
+                var document = new XmlDocument();
+                document.Load(xmlReader);
+
+                var namespaceManager = new XmlNamespaceManager(document.NameTable);
+                foreach (var xmlNamespace in Settings.Namespaces)
+                {
+                    namespaceManager.AddNamespace(xmlNamespace.Key /* Prefix */, xmlNamespace.Value /* URI */);
+                }
+
+                var nodes = document.SelectNodes(xpath, namespaceManager);
+                return nodes.Cast<XmlNode>().All(node => node.Value == value);
+            }
+        }
+
+        public bool TestIsRemoved(string xpath)
+        {
+            var xmlString = new StreamReader(FileSystem.GetFile(XmlPath).OpenRead()).ReadToEnd();
+            return TestIsRemoved(xmlString, xpath);
+        }
+
+        public bool TestIsRemoved(string xml, string xpath)
+        {
+            using (var reader = new StringReader(xml))
+            using (var xmlReader = XmlReader.Create(reader))
+            {
+
+                var document = new XmlDocument();
+                document.Load(xmlReader);
+
+                var namespaceManager = new XmlNamespaceManager(document.NameTable);
+                foreach (var xmlNamespace in Settings.Namespaces)
+                {
+                    namespaceManager.AddNamespace(xmlNamespace.Key /* Prefix */, xmlNamespace.Value /* URI */);
+                }
+
+                var nodes = document.SelectNodes(xpath, namespaceManager);
+                return nodes.Count == 0;
+            }
+        }
+    }
+}
---
+++ src/Cake.Common.Tests/Unit/XML/XmlPokeTests.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+
+using Cake.Common.Tests.Fixtures;
+
+using Xunit;
+
+namespace Cake.Common.Tests.Unit.XML
+{
+    public sealed class XmlPokeTests
+    {
+        public sealed class ValidateParameters
+        {
+            [Fact]
+            public void Should_Throw_If_FilePath_Is_Null()
+            {
+                // Given
+                var fixture = new XmlPokeFixture(false);
+
+                // When
+                var result = Record.Exception(() => fixture.Poke("gibblygook", ""));
+
+                // Then
+                Assert.IsArgumentNullException(result, "filePath");
+            }
+
+            [Fact]
+            public void Should_Throw_If_File_Doesnt_Exists()
+            {
+                // Given
+                var fixture = new XmlPokeFixture(false);
+                fixture.XmlPath = "/Working/web.config";
+
+                // When
+                var result = Record.Exception(() => fixture.Poke("gibblygook", ""));
+
+                // Then
+                Assert.IsType<FileNotFoundException>(result);
+            }
+
+            [Fact]
+            public void Should_Throw_If_No_Xpath()
+            {
+                // Given
+                var fixture = new XmlPokeFixture();
+
+                // When
+                var result = Record.Exception(() => fixture.Poke(null, ""));
+
+                // Then
+                Assert.IsArgumentNullException(result, "xpath");
+            }
+        }
+
+        public sealed class Transform
+        {
+            [Fact]
+            public void Should_Change_Attribute()
+            {
+                // Given
+                var fixture = new XmlPokeFixture();
+
+                // When
+                fixture.Poke("/configuration/appSettings/add[@key = 'server']/@value", "productionhost.somecompany.com");
+
+                // Then
+                fixture.TestIsValue(
+                    "/configuration/appSettings/add[@key = 'server']/@value",
+                    "productionhost.somecompany.com");
+            }
+
+            [Fact]
+            public void Should_Remove_Attribute()
+            {
+                // Given
+                var fixture = new XmlPokeFixture();
+
+                // When
+                fixture.Poke("/configuration/appSettings/add[@key = 'server']", null);
+
+                // Then
+                fixture.TestIsRemoved(
+                    "/configuration/appSettings/add[@key = 'server']");
+            }
+        }
+    }
+}
