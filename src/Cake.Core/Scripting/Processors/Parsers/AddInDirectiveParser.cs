6fc39155ca8c62d4f455624d61e34f51d79a7bc0
Mark Walker <mark@walkersretreat.co.nz>
2015-08-27 12:53:41 +1200

    Split out Add-in directive parsing as pre-cursor to #373

---
+++ src/Cake.Core/Scripting/Processors/Parsers/AddInDirectiveParser.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using Cake.Core.Diagnostics;
+using Cake.Core.IO;
+
+namespace Cake.Core.Scripting.Processors.Parsers
+{
+  /// <summary>
+  /// Parses the information from an #addin line
+  /// </summary>
+  public class AddInDirectiveParser
+  {
+    private readonly ICakeEnvironment _environment;
+    private readonly IFileSystem _fileSystem;
+    private readonly ICakeLog _log;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AddInDirectiveParser" /> class.
+    /// </summary>
+    /// <param name="fileSystem">The file system.</param>
+    /// <param name="environment">The environment.</param>
+    /// <param name="log">The logger</param>
+    public AddInDirectiveParser(ICakeEnvironment environment, IFileSystem fileSystem, ICakeLog log)
+    {
+      _environment = environment;
+      _fileSystem = fileSystem;
+      _log = log;
+    }
+
+    /// <summary>
+    /// Parses the information from an #addin line
+    /// </summary>
+    /// <param name="tokens">The #addin line split by space (' ')</param>
+    /// <returns>Populated AddInDirectiveArguments</returns>
+    public AddInDirectiveArguments Parse(string[] tokens)
+    {
+      var arguments = new AddInDirectiveArguments();
+      try
+      {
+        if (tokens != null && tokens.Any())
+        {
+          var directive = tokens.FirstOrDefault();
+          if (!string.IsNullOrWhiteSpace(directive))
+          {
+            if (directive.Equals("#addin", StringComparison.OrdinalIgnoreCase))
+            {
+              // Fetch the addin NuGet ID.
+              arguments.AddInId = tokens
+                .Select(value => value.UnQuote())
+                .Skip(1).FirstOrDefault();
+
+              if (!string.IsNullOrWhiteSpace(arguments.AddInId))
+              {
+                // Fetch optional NuGet source.
+                arguments.Source = tokens
+                  .Skip(2)
+                  .Select(value => value.UnQuote())
+                  .FirstOrDefault();
+
+                // Get the directory path to Cake.
+                var applicationRoot = _environment.GetApplicationRoot();
+
+                // Get the addin directory.
+                var addInRootDirectoryPath = applicationRoot
+                  .Combine("..\\Addins")
+                  .Collapse()
+                  .MakeAbsolute(_environment);
+
+                arguments.AddInDirectoryPath = addInRootDirectoryPath.Combine(arguments.AddInId);
+                arguments.AddInRootDirectory = _fileSystem.GetDirectory(addInRootDirectoryPath);
+                arguments.Valid = true;
+              }
+            }
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        // Log this error.
+        const string format = "An error occured while parsing line {0}.";
+        _log.Error(format, string.Join(" ", tokens));
+        _log.Error("Error: {0}", _log.Verbosity == Verbosity.Diagnostic ? e.ToString() : e.Message);
+        arguments.Valid = false;
+      }
+
+      return arguments;
+    }
+
+    /// <summary>
+    /// Add In Directive Arguments
+    /// </summary>
+    public class AddInDirectiveArguments
+    {
+      /// <summary>
+      /// Gets or sets a the identifier for this add-in
+      /// </summary>
+      public string AddInId { get; set; }
+
+      /// <summary>
+      /// Gets or sets a the add-in source (Nuget.org used if missing)
+      /// </summary>
+      public string Source { get; set; }
+
+      /// <summary>
+      /// Gets or sets a path to this add-in's install path
+      /// </summary>
+      public DirectoryPath AddInDirectoryPath { get; set; }
+
+      /// <summary>
+      /// Gets or sets a path to parent path to all add-in installs
+      /// </summary>
+      public IDirectory AddInRootDirectory { get; set; }
+
+      /// <summary>
+      /// Gets or sets a value indicating whether the line was successfully parsed as an add-in
+      /// </summary>
+      public bool Valid { get; set; }
+    }
+  }
+
+
+}
---
+++ src/Cake.Tests/Unit/Scripting/AddIns/Parser/AddinDirectiveParserTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using Cake.Core.Scripting.Processors.Parsers;
+using Cake.Tests.Fixtures;
+using Xunit;
+
+namespace Cake.Tests.Unit.Scripting.AddIns.Parser
+{
+  public class AddinDirectiveParserTests
+  {
+    public class TheParseMethod
+    {
+      [Fact]
+      public void Should_Return_False_If_Arguments_Are_Null()
+      {
+        // Given
+        var fixture = new AddInDirectiveParserFixture();
+        var parser = new AddInDirectiveParser(fixture.Environment, fixture.FileSystem, fixture.Log);
+
+        // When
+        var result = parser.Parse(null);
+
+        // Then
+        Assert.False(result.Valid);
+      }
+
+      [Fact]
+      public void Can_Parse_Empty_Parameters()
+      {
+        // Given
+        var fixture = new AddInDirectiveParserFixture();
+        var parser = new AddInDirectiveParser(fixture.Environment, fixture.FileSystem, fixture.Log);
+
+        // When
+        var result = parser.Parse(new string[] { });
+
+        // Then
+        Assert.NotNull(result);
+        Assert.False(result.Valid);
+      }
+
+      [Theory]
+      [InlineData("#addin xxx", "xxx")]
+      [InlineData("#addin yyy", "yyy")]
+      public void Can_Parse_AddinId(string input, string addinId)
+      {
+        // Given
+        var fixture = new AddInDirectiveParserFixture();
+        var parser = new AddInDirectiveParser(fixture.Environment, fixture.FileSystem, fixture.Log);
+        var tokens = input.Split(new[] { ' ' }, StringSplitOptions.None);
+
+        // When
+        var result = parser.Parse(tokens);
+
+        // Then
+        Assert.NotNull(result);
+        Assert.True(result.Valid);
+        Assert.Equal(addinId, result.AddInId);
+      }
+
+      [Theory]
+      [InlineData("#addin xxx http://myserver.org/", "xxx", "http://myserver.org/")]
+      [InlineData("#addin yyy http://yourserver.org/", "yyy", "http://yourserver.org/")]
+      public void Can_Parse_Source(string input, string addinId, string source)
+      {
+        // Given
+        var fixture = new AddInDirectiveParserFixture();
+        var parser = new AddInDirectiveParser(fixture.Environment, fixture.FileSystem, fixture.Log);
+        var tokens = input.Split(new[] { ' ' }, StringSplitOptions.None);
+
+        // When
+        var result = parser.Parse(tokens);
+
+        // Then
+        Assert.NotNull(result);
+        Assert.True(result.Valid);
+        Assert.Equal(addinId, result.AddInId);
+        Assert.Equal(source, result.Source);
+      }
+    }
+  }
+}
