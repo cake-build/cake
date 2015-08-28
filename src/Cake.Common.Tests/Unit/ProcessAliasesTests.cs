0e2b14586bbc2843942954c1ba457f32bbd96f3a
James Nail <jrnail23@gmail.com>
2015-08-28 12:56:47 -0400

    Extended ProcessAliases with methods returning IProcess -- renamed StartNewProcess to StartAndReturnProcess, per feedback from @patriksvensson

--- src/Cake.Common.Tests/Unit/ProcessAliasesTests.cs
+++ src/Cake.Common.Tests/Unit/ProcessAliasesTests.cs
@@ -227,5 +227,229 @@ namespace Cake.Common.Tests.Unit
                 }
             }
         }
+
+        public sealed class TheStartAndReturnProcessMethod
+        {
+            public sealed class WithoutSettings
+            {
+                [Fact]
+                public void Should_Throw_If_Context_Is_Null()
+                {
+                    // Given
+                    const string fileName = "git";
+
+                    // When
+                    var result = Record.Exception(() =>
+                        ProcessAliases.StartAndReturnProcess(null, fileName));
+
+                    // Then
+                    Assert.IsArgumentNullException(result, "context");
+                }
+
+                [Fact]
+                public void Should_Throw_If_Filename_Is_Null()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+
+                    // When
+                    var result = Record.Exception(() =>
+                        context.StartAndReturnProcess(null));
+
+                    // Then
+                    Assert.IsArgumentNullException(result, "fileName");
+                }
+
+                [Fact]
+                public void Should_Use_Environments_Working_Directory()
+                {
+                    // Given
+                    var fixture = new ProcessFixture();
+
+                    // When
+                    fixture.StartNewProcess("hello.exe");
+
+                    // Then
+                    fixture.ProcessRunner.Received(1).Start(
+                        Arg.Any<FilePath>(), Arg.Is<ProcessSettings>(info =>
+                            info.WorkingDirectory.FullPath == "/Working"));
+                }
+
+                [Fact]
+                public void Should_Throw_If_No_Process_Was_Returned_From_Process_Runner()
+                {
+                    // Given
+                    var fixture = new ProcessFixture();
+                    fixture.ProcessRunner.Start(Arg.Any<FilePath>(), Arg.Any<ProcessSettings>())
+                        .Returns((IProcess)null);
+
+                    // When
+                    var result = Record.Exception(() => fixture.StartNewProcess("hello.exe"));
+
+                    // Then
+                    Assert.IsType<CakeException>(result);
+                    Assert.Equal("Could not start process.", result.Message);
+                }
+
+                [Fact]
+                public void Should_Return_Process_Started_By_ProcessRunner()
+                {
+                    // Given
+                    var fixture = new ProcessFixture();
+                    const string fileName = "hello.exe";
+                    
+                    var expected = fixture.Process;
+                    fixture.ProcessRunner.Start(Arg.Any<FilePath>(), Arg.Any<ProcessSettings>()).Returns(p => expected);
+
+                    // When
+                    var result = fixture.StartNewProcess(fileName);
+
+                    // Then
+                    Assert.Same(expected, result);
+                }
+            }
+
+            public sealed class WithSettings
+            {
+                [Fact]
+                public void Should_Throw_If_Context_Is_Null()
+                {
+                    // Given
+                    const string fileName = "git";
+                    var settings = new ProcessSettings();
+
+                    // When
+                    var result = Record.Exception(() =>
+                        ProcessAliases.StartAndReturnProcess(null, fileName, settings));
+
+                    // Then
+                    Assert.IsArgumentNullException(result, "context");
+                }
+
+                [Fact]
+                public void Should_Throw_If_Filename_Is_Null()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+                    var settings = new ProcessSettings();
+
+                    // When
+                    var result = Record.Exception(() =>
+                        context.StartAndReturnProcess(null, settings));
+
+                    // Then
+                    Assert.IsArgumentNullException(result, "fileName");
+                }
+
+                [Fact]
+                public void Should_Throw_If_Settings_Are_Null()
+                {
+                    // Given
+                    var context = Substitute.For<ICakeContext>();
+                    const string fileName = "git";
+
+                    // When
+                    var result = Record.Exception(() =>
+                        context.StartAndReturnProcess(fileName, null));
+
+                    // Then
+                    Assert.IsArgumentNullException(result, "settings");
+                }
+
+                [Fact]
+                public void Should_Use_Environments_Working_Directory_If_Not_Working_Directory_Is_Set()
+                {
+                    // Given
+                    var fixture = new ProcessFixture();
+                    const string fileName = "hello.exe";
+                    var settings = new ProcessSettings();
+
+                    // When
+                    fixture.StartNewProcess(fileName, settings);
+
+                    // Then
+                    fixture.ProcessRunner.Received(1).Start(
+                        Arg.Any<FilePath>(),
+                        Arg.Is<ProcessSettings>(info =>
+                            info.WorkingDirectory.FullPath == "/Working"));
+                }
+
+                [Fact]
+                public void Should_Use_Provided_Working_Directory_If_Set()
+                {
+                    // Given
+                    var fixture = new ProcessFixture();
+                    const string fileName = "hello.exe";
+                    var settings = new ProcessSettings();
+                    settings.WorkingDirectory = "/OtherWorking";
+
+                    // When
+                    fixture.StartNewProcess(fileName, settings);
+
+                    // Then
+                    fixture.ProcessRunner.Received(1).Start(
+                        Arg.Any<FilePath>(),
+                        Arg.Is<ProcessSettings>(info =>
+                            info.WorkingDirectory.FullPath == "/OtherWorking"));
+                }
+
+                [Fact]
+                public void Should_Make_Working_Directory_Absolute_If_Set()
+                {
+                    // Given
+                    var fixture = new ProcessFixture();
+                    const string fileName = "hello.exe";
+                    var settings = new ProcessSettings();
+                    settings.WorkingDirectory = "OtherWorking";
+
+                    // When
+                    fixture.StartNewProcess(fileName, settings);
+
+                    // Then
+                    fixture.ProcessRunner.Received(1).Start(
+                        Arg.Any<FilePath>(),
+                        Arg.Is<ProcessSettings>(info =>
+                            info.WorkingDirectory.FullPath == "/Working/OtherWorking"));
+                }
+
+                [Fact]
+                public void Should_Throw_If_No_Process_Was_Returned_From_Process_Runner()
+                {
+                    // Given
+                    var fixture = new ProcessFixture();
+                    const string fileName = "hello.exe";
+                    var settings = new ProcessSettings();
+
+                    fixture.ProcessRunner.Start(
+                        Arg.Any<FilePath>(),
+                        Arg.Any<ProcessSettings>()).Returns((IProcess)null);
+
+                    // When
+                    var result = Record.Exception(() => fixture.StartNewProcess(fileName, settings));
+
+                    // Then
+                    Assert.IsType<CakeException>(result);
+                    Assert.Equal("Could not start process.", result.Message);
+                }
+
+                [Fact]
+                public void Should_Return_Process_Started_By_ProcessRunner()
+                {
+                    // Given
+                    var fixture = new ProcessFixture();
+                    const string fileName = "hello.exe";
+                    var settings = new ProcessSettings();
+
+                    var expected = fixture.Process;
+                    fixture.ProcessRunner.Start(Arg.Any<FilePath>(), Arg.Any<ProcessSettings>()).Returns(p => expected);
+
+                    // When
+                    var result = fixture.StartNewProcess(fileName, settings);
+
+                    // Then
+                    Assert.IsAssignableFrom<IProcess>(expected);
+                }
+            }
+        }
     }
 }
