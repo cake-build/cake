9f03564aa3325ee6efd876060d8b999852edac03
Gary Ewan Park <gep13@gep13.co.uk>
2015-10-12 12:31:39 +0100

    (GH-446) Adding GitReleaseManager AddAssets Command with Tests

---
+++ src/Cake.Common.Tests/Fixtures/Tools/GitReleaseManager/GitReleaseManagerAssetsAdderFixture.cs
@@ -0,0 +1,32 @@
+﻿using Cake.Common.Tools.GitReleaseManager.AddAssets;
+
+namespace Cake.Common.Tests.Fixtures.Tools.GitReleaseManager
+{
+    public sealed class GitReleaseManagerAssetsAdderFixture : GitReleaseManagerFixture
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Owner { get; set; }
+        public string Repository { get; set; }
+        public string TagName { get; set; }
+        public string Assets { get; set; }
+        public GitReleaseManagerAddAssetsSettings Settings { get; set; }
+
+        public GitReleaseManagerAssetsAdderFixture()
+        {
+            UserName = "bob";
+            Password = "password";
+            Owner = "repoOwner";
+            Repository = "repo";
+            TagName = "0.1.0";
+            Assets = @"c:/temp/asset1.txt";
+            Settings = new GitReleaseManagerAddAssetsSettings();
+        }
+
+        public void AddAssets()
+        {
+            var tool = new GitReleaseManagerAssetsAdder(FileSystem, Environment, ProcessRunner, Globber, GitReleaseManagerToolResolver);
+            tool.AddAssets(UserName, Password, Owner, Repository, TagName, Assets, Settings);
+        }
+    }
+}
--- src/Cake.Common/Tools/GitReleaseManager/GitReleaseManagerAliases.cs
+++ src/Cake.Common/Tools/GitReleaseManager/GitReleaseManagerAliases.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Cake.Common.Tools.GitReleaseManager.AddAssets;
 using Cake.Common.Tools.GitReleaseManager.Create;
 using Cake.Core;
 using Cake.Core.Annotations;
@@ -33,6 +34,32 @@ namespace Cake.Common.Tools.GitReleaseManager
             var resolver = new GitReleaseManagerToolResolver(context.FileSystem, context.Environment, context.Globber);
             var creator = new GitReleaseManagerCreator(context.FileSystem, context.Environment, context.ProcessRunner, context.Globber, resolver);
             creator.Create(userName, password, owner, repository, settings);
+        }
+
+        /// <summary>
+        /// Add Assets to an existing release using the specified settings.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="userName">The user name.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="owner">The owner.</param>
+        /// <param name="repository">The repository.</param>
+        /// <param name="tagName">The tag name.</param>
+        /// <param name="assets">The assets.</param>
+        /// <param name="settings">The settings.</param>
+        [CakeMethodAlias]
+        [CakeAliasCategory("AddAssets")]
+        [CakeNamespaceImport("Cake.Common.Tools.GitReleaseManager.AddAssets")]
+        public static void GitReleaseManagerAddAssets(this ICakeContext context, string userName, string password, string owner, string repository, string tagName, string assets, GitReleaseManagerAddAssetsSettings settings)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var resolver = new GitReleaseManagerToolResolver(context.FileSystem, context.Environment, context.Globber);
+            var assetsAdder = new GitReleaseManagerAssetsAdder(context.FileSystem, context.Environment, context.ProcessRunner, context.Globber, resolver);
+            assetsAdder.AddAssets(userName, password, owner, repository, tagName, assets, settings);
         }
     }
 }
