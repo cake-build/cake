fb22ccfc4e58d78e1f22d885880fc280d56bace4
Gary Ewan Park <gep13@gep13.co.uk>
2015-10-16 21:32:51 +0100

    (GH-448) Added GitVersion Support with Tests

---
+++ src/Cake.Common/Tools/GitVersion/GitVersion.cs
@@ -0,0 +1,98 @@
+﻿namespace Cake.Common.Tools.GitVersion
+{
+    /// <summary>
+    /// GitVersion information
+    /// </summary>
+    public sealed class GitVersion
+    {
+        /// <summary>
+        /// Gets or sets the major version.
+        /// </summary>
+        public int Major { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minor version.
+        /// </summary>
+        public int Minor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the patch version.
+        /// </summary>
+        public int Patch { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pre-release tag.
+        /// </summary>
+        public string PreReleaseTag { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pre-release tag with dash.
+        /// </summary>
+        public string PreReleaseTagWithDash { get; set; }
+
+        /// <summary>
+        /// Gets or sets the build metadata.
+        /// </summary>
+        public string BuildMetaData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the major version.
+        /// </summary>
+        public string FullBuildMetaData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the major, minor, and path.
+        /// </summary>
+        public string MajorMinorPatch { get; set; }
+
+        /// <summary>
+        /// Gets or sets the semver.
+        /// </summary>
+        public string SemVer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the legacy semver.
+        /// </summary>
+        public string LegacySemVer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the padded legacy semver.
+        /// </summary>
+        public string LegacySemVerPadded { get; set; }
+
+        /// <summary>
+        /// Gets or sets the assembly semver.
+        /// </summary>
+        public string AssemblySemVer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full semver.
+        /// </summary>
+        public string FullSemVer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the informational version.
+        /// </summary>
+        public string InformationalVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the branch name.
+        /// </summary>
+        public string BranchName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the git sha.
+        /// </summary>
+        public string Sha { get; set; }
+
+        /// <summary>
+        /// Gets or sets the nuget version for v2.
+        /// </summary>
+        public string NuGetVersionV2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the nuget version.
+        /// </summary>
+        public string NuGetVersion { get; set; }
+    }
+}
