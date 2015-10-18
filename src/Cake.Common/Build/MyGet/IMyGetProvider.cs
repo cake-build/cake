8d4ba6d42e7da7151bc5b47492aa2ce24759ea3f
Gary Ewan Park <gep13@gep13.co.uk>
2015-10-18 16:28:14 +0100

    (GH-148) Add MyGet Build System Support
    As documented here:
    
    http://docs.myget.org/docs/reference/build-services

---
+++ src/Cake.Common/Build/MyGet/IMyGetProvider.cs
@@ -0,0 +1,43 @@
+﻿namespace Cake.Common.Build.MyGet
+{
+    /// <summary>
+    /// Represents a MyGet provider.
+    /// </summary>
+    public interface IMyGetProvider
+    {
+        /// <summary>
+        /// Gets a value indicating whether the current build is running on MyGet.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if the current build is running on MyGet; otherwise, <c>false</c>.
+        /// </value>
+        bool IsRunningOnMyGet { get; }
+
+        /// <summary>
+        /// Report a build problem to MyGet.
+        /// </summary>
+        /// <param name="description">Description of build problem.</param>
+        void BuildProblem(string description);
+
+        /// <summary>
+        /// Allows setting an environment variable that can be used by a future process.
+        /// </summary>
+        /// <param name="name">Name of the parameter to set.</param>
+        /// <param name="value">Value to assign to the parameter.</param>
+        void SetParameter(string name, string value);
+
+        /// <summary>
+        /// Write a status message to the MyGet build log.
+        /// </summary>
+        /// <param name="message">Message contents.</param>
+        /// <param name="status">Build status.</param>
+        /// <param name="errorDetails">Error details if status is error.</param>
+        void WriteStatus(string message, MyGetBuildStatus status, string errorDetails = null);
+
+        /// <summary>
+        /// Tells MyGet to change the current build number.
+        /// </summary>
+        /// <param name="buildNumber">The required build number.</param>
+        void SetBuildNumber(string buildNumber);
+    }
+}
