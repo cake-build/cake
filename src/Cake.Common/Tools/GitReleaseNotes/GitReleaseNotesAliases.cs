cc61a3e514bfda3ef16e1524c65b94d1639d4163
Gary Ewan Park <gep13@gep13.co.uk>
2015-10-19 21:13:51 +0100

    (GH-460) Added GitReleaseNotes Support with Tests

---
+++ src/Cake.Common/Tools/GitReleaseNotes/GitReleaseNotesAliases.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+
+namespace Cake.Common.Tools.GitReleaseNotes
+{
+    /// <summary>
+    ///  Contains functionality related to ReSharper's duplication finder
+    /// </summary>
+    [CakeAliasCategory("GitReleaseNotes")]
+    public static class GitReleaseNotesAliases
+    {
+        /// <summary>
+        /// Generates a set of release notes based on the commit history of the repository and specified settings.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="outputFile">The output file.</param>
+        /// <param name="settings">The settings.</param>
+        [CakeMethodAlias]
+        [CakeAliasCategory("GitReleaseNotes")]
+        public static void GitReleaseNotes(this ICakeContext context, FilePath outputFile, GitReleaseNotesSettings settings)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var runner = new GitReleaseNotesRunner(context.FileSystem, context.Environment, context.ProcessRunner, context.Globber);
+            runner.Run(outputFile, settings);
+        }
+    }
+}
---
+++ src/Cake.Common/Tools/GitReleaseNotes/GitReleaseNotesIssueTracker.cs
@@ -0,0 +1,28 @@
+﻿namespace Cake.Common.Tools.GitReleaseNotes
+{
+    /// <summary>
+    /// The IssueTracker to be used.
+    /// </summary>
+    public enum GitReleaseNotesIssueTracker
+    {
+        /// <summary>
+        /// Uses BitBucket as Issue Tracker.
+        /// </summary>
+        BitBucket,
+
+        /// <summary>
+        /// Uses GitHub as Issue Tracker.
+        /// </summary>
+        GitHub,
+
+        /// <summary>
+        /// Uses Jira as Issue Tracker.
+        /// </summary>
+        Jira,
+
+        /// <summary>
+        /// Uses YouTrack as Issue Tracker.
+        /// </summary>
+        YouTrack
+    }
+}
