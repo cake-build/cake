54b204c59ec898e46b0ce6e79f406e3d9eef7169
Yves Schmid <yvschmid@gmail.com>
2015-10-22 20:57:30 +0200

    Adds ReportGenerator support

---
+++ src/Cake.Common.Tests/Fixtures/Tools/ReportGeneratorRunnerFixture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Cake.Common.Tools.ReportGenerator;
+using Cake.Core.IO;
+
+namespace Cake.Common.Tests.Fixtures.Tools
+{
+    internal sealed class ReportGeneratorRunnerFixture : ToolFixture<ReportGeneratorSettings>
+    {
+        public IEnumerable<FilePath> Reports { get; set; }
+        public DirectoryPath TargetDir { get; set; }
+
+        public ReportGeneratorRunnerFixture() : base("ReportGenerator.exe")
+        {
+            Reports = new FilePath[] { "report.xml" };
+            TargetDir = "output";
+        }
+
+        protected override void RunTool()
+        {
+            var tool = new ReportGeneratorRunner(FileSystem, Environment, ProcessRunner, Globber);
+            tool.Run(Reports, TargetDir, Settings);
+        }
+    }
+}
---
+++ src/Cake.Common/Tools/ReportGenerator/ReportGeneratorSettings.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Cake.Core.IO;
+using Cake.Core.Tooling;
+
+namespace Cake.Common.Tools.ReportGenerator
+{
+    /// <summary>
+    /// Contains settings used by <see cref="ReportGeneratorRunner"/>.
+    /// </summary>
+    public sealed class ReportGeneratorSettings : ToolSettings
+    {
+        /// <summary>
+        /// Gets or sets the list of coverage reports that should be parsed.
+        /// </summary>
+        public ICollection<ReportGeneratorReportType> ReportTypes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the directories which contain the corresponding source code.
+        /// The source files are used if coverage report contains classes without path information.
+        /// </summary>
+        public ICollection<DirectoryPath> SourceDirectories { get; set; }
+
+        /// <summary>
+        /// Gets or sets the directory for storing persistent coverage information.
+        /// Can be used in future reports to show coverage evolution.
+        /// </summary>
+        public DirectoryPath HistoryDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of assemblies that should be included or excluded in the report.
+        /// Exclusion filters take precedence over inclusion filters.
+        /// Wildcards are allowed.
+        /// </summary>
+        public ICollection<string> AssemblyFilters { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of classes that should be included or excluded in the report.
+        /// Exclusion filters take precedence over inclusion filters.
+        /// Wildcards are allowed.
+        /// </summary>
+        public ICollection<string> ClassFilters { get; set; }
+
+        /// <summary>
+        /// Gets or sets the verbosity level of the log messages.
+        /// </summary>
+        public ReportGeneratorVerbosity? Verbosity { get; set; }
+    }
+}
