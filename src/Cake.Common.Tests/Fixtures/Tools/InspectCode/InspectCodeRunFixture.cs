425d19decbcfe9be4f716e8dacabbf8cc41a43d8
Gary Ewan Park <gep13@gep13.co.uk>
2015-11-05 20:33:40 +0000

    Add ability to fail build as a result of finding violations

--- src/Cake.Common.Tests/Fixtures/Tools/InspectCode/InspectCodeRunFixture.cs
+++ src/Cake.Common.Tests/Fixtures/Tools/InspectCode/InspectCodeRunFixture.cs
@@ -1,20 +1,32 @@
+using Cake.Common.Tests.Properties;
 using Cake.Common.Tools.InspectCode;
+using Cake.Core;
+using Cake.Core.Diagnostics;
 using Cake.Core.IO;
+using Cake.Testing;
+using NSubstitute;
 
 namespace Cake.Common.Tests.Fixtures.Tools.InspectCode
 {
     internal sealed class InspectCodeRunFixture : InspectCodeFixture
     {
+        private readonly ICakeLog Log;
+
         public FilePath Solution { get; set; }
 
         public InspectCodeRunFixture()
         {
             Solution = new FilePath("./Test.sln");
+
+            Log = Substitute.For<ICakeLog>();
+
+            FileSystem.CreateFile("build/inspect_code.xml").SetContent(Resources.InspectCodeReportNoViolations.NormalizeLineEndings());
+            FileSystem.CreateFile("build/violations.xml").SetContent(Resources.InspectCodeReportWithViolations.NormalizeLineEndings());
         }
 
         protected override void RunTool()
         {
-            var tool = new InspectCodeRunner(FileSystem, Environment, ProcessRunner, Globber);
+            var tool = new InspectCodeRunner(FileSystem, Environment, ProcessRunner, Globber, Log);
             tool.Run(Solution, Settings);
         }
     }
--- src/Cake.Common.Tests/Fixtures/Tools/InspectCode/InspectCodeRunFromConfigFixture.cs
+++ src/Cake.Common.Tests/Fixtures/Tools/InspectCode/InspectCodeRunFromConfigFixture.cs
@@ -1,15 +1,24 @@
 ﻿using Cake.Common.Tools.InspectCode;
+using Cake.Core.Diagnostics;
 using Cake.Core.IO;
+using NSubstitute;
 
 namespace Cake.Common.Tests.Fixtures.Tools.InspectCode
 {
     internal sealed class InspectCodeRunFromConfigFixture : InspectCodeFixture
     {
+        private readonly ICakeLog Log;
+
         public FilePath Config { get; set; }
+
+        public InspectCodeRunFromConfigFixture()
+        {
+            Log = Substitute.For<ICakeLog>();
+        }
 
         protected override void RunTool()
         {
-            var tool = new InspectCodeRunner(FileSystem, Environment, ProcessRunner, Globber);
+            var tool = new InspectCodeRunner(FileSystem, Environment, ProcessRunner, Globber, Log);
             tool.RunFromConfig(Config);
         }
     }
