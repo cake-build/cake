897d2129d9651047467a29b5e446aec061be6c9c
Joris de Bock <jorisdebock7@gmail.com>
2015-11-26 21:22:27 +0100

    Added DNU tool support.

---
+++ src/Cake.Common.Tests/Fixtures/Tools/DNU/Build/DNUBuilderFixture.cs
@@ -0,0 +1,15 @@
+﻿using Cake.Common.Tools.DNU.Build;
+
+namespace Cake.Common.Tests.Fixtures.Tools.DNU.Build
+{
+    internal sealed class DNUBuilderFixture : DNUFixture<DNUBuildSettings>
+    {
+        public string Path { get; set; }
+
+        protected override void RunTool()
+        {
+            var tool = new DNUBuilder(FileSystem, Environment, ProcessRunner, Globber);
+            tool.Build(Path, Settings);
+        }
+    }
+}
---
+++ src/Cake.Common.Tests/Unit/Tools/DNU/Pack/DNUPackerTests.cs
@@ -0,0 +1,200 @@
+﻿using Cake.Common.Tests.Fixtures.Tools.DNU.Pack;
+using Cake.Common.Tools.DNU.Pack;
+using Xunit;
+
+namespace Cake.Common.Tests.Unit.Tools.DNU.Pack
+{
+    public sealed class DNUPackerTests
+    {
+        public sealed class ThePackMethod
+        {
+            [Fact]
+            public void Should_Throw_If_Settings_Are_Null()
+            {
+                // Given
+                var fixture = new DNUPackerFixture();
+                fixture.Path = "./src/*";
+                fixture.Settings = null;
+                fixture.GivenDefaultToolDoNotExist();
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                Assert.IsArgumentNullException(result, "settings");
+            }
+
+            [Fact]
+            public void Should_Throw_If_Path_Are_Null()
+            {
+                // Given
+                var fixture = new DNUPackerFixture();
+                fixture.Settings = new DNUPackSettings();
+                fixture.GivenDefaultToolDoNotExist();
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                Assert.IsArgumentNullException(result, "path");
+            }
+
+            [Fact]
+            public void Should_Throw_If_DNU_Executable_Was_Not_Found()
+            {
+                // Given
+                var fixture = new DNUPackerFixture();
+                fixture.Path = "./src/*";
+                fixture.GivenDefaultToolDoNotExist();
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                Assert.IsCakeException(result, "DNU: Could not locate executable.");
+            }
+
+            [Fact]
+            public void Should_Throw_If_Process_Was_Not_Started()
+            {
+                // Given
+                var fixture = new DNUPackerFixture();
+                fixture.Path = "./src/*";
+                fixture.GivenProcessCannotStart();
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                Assert.IsCakeException(result, "DNU: Process was not started.");
+            }
+
+            [Fact]
+            public void Should_Throw_If_Process_Has_A_Non_Zero_Exit_Code()
+            {
+                // Given
+                var fixture = new DNUPackerFixture();
+                fixture.Path = "./src/*";
+                fixture.GivenProcessExitsWithCode(1);
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                Assert.IsCakeException(result, "DNU: Process returned an error.");
+            }
+
+            [Fact]
+            public void Should_Add_Mandatory_Arguments()
+            {
+                // Given
+                var fixture = new DNUPackerFixture();
+                fixture.Path = "./src/*";
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("pack \"./src/*\"", result.Args);
+            }
+
+            [Fact]
+            public void Should_Add_Frameworks_If_Set()
+            {
+                // Given
+                var fixture = new DNUPackerFixture();
+                fixture.Path = "./src/*";
+                fixture.Settings = new DNUPackSettings
+                {
+                    Frameworks = new[] { "dnx451", "dnxcore50" }
+                };
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("pack \"./src/*\" --framework \"dnx451;dnxcore50\"", result.Args);
+            }
+
+            [Fact]
+            public void Should_Add_Configurations_If_Set()
+            {
+                // Given
+                var fixture = new DNUPackerFixture();
+                fixture.Path = "./src/*";
+                fixture.Settings = new DNUPackSettings
+                {
+                    Configurations = new[] { "Debug", "Release" }
+                };
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("pack \"./src/*\" --configuration \"Debug;Release\"", result.Args);
+            }
+
+            [Fact]
+            public void Should_Add_Proxy_If_Set()
+            {
+                // Given
+                var fixture = new DNUPackerFixture();
+                fixture.Path = "./src/*";
+                fixture.Settings = new DNUPackSettings
+                {
+                    OutputDirectory = "./artifacts/"
+                };
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("pack \"./src/*\" --out \"/Working/artifacts\"", result.Args);
+            }
+
+            [Fact]
+            public void Should_Add_Quiet_If_Set()
+            {
+                // Given
+                var fixture = new DNUPackerFixture();
+                fixture.Path = "./src/*";
+                fixture.Settings = new DNUPackSettings
+                {
+                    Quiet = true
+                };
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("pack \"./src/*\" --quiet", result.Args);
+            }
+
+            [Fact]
+            public void Should_Add_Arguments_And_Settings_If_Set()
+            {
+                // Given
+                var fixture = new DNUPackerFixture();
+                fixture.Path = "./src/*";
+                fixture.Settings = new DNUPackSettings
+                {
+                    Frameworks = new[] { "dnx451", "dnxcore50" },
+                    Configurations = new[] { "Debug", "Release" },
+                    OutputDirectory = "./artifacts/",
+                    Quiet = true
+                };
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("pack \"./src/*\"" +
+                             " --framework \"dnx451;dnxcore50\"" +
+                             " --configuration \"Debug;Release\"" +
+                             " --out \"/Working/artifacts\"" +
+                             " --quiet",
+                    result.Args);
+            }
+        }
+    }
+}
