6f1be7b115fc38e9c542d5e2d29f0c9d80971b8c
Patrik Svensson <patrik.svensson@tobii.com>
2015-12-28 23:17:35 +0100

    Added Win32 platform target.
    Closes #604

--- src/Cake.Common/Tools/MSBuild/PlatformTarget.cs
+++ src/Cake.Common/Tools/MSBuild/PlatformTarget.cs
@@ -6,26 +6,31 @@
     public enum PlatformTarget
     {
         /// <summary>
-        /// PlatformTarget: <c>MSIL</c> (AnyCPU)
+        /// Platform target: <c>MSIL</c> (Any CPU)
         /// </summary>
         MSIL = 0,
 
         /// <summary>
-        /// PlatformTarget: <c>x86</c>
+        /// Platform target: <c>x86</c>
         /// </summary>
         // ReSharper disable once InconsistentNaming
         x86 = 1,
 
         /// <summary>
-        /// PlatformTarget: <c>x64</c>
+        /// Platform target: <c>x64</c>
         /// </summary>
         // ReSharper disable once InconsistentNaming
         x64 = 2,
 
         /// <summary>
-        /// PlatformTarget: <c>ARM</c>
+        /// Platform target: <c>ARM</c>
         /// </summary>
         // ReSharper disable once InconsistentNaming
         ARM = 3,
+
+        /// <summary>
+        /// Platform target: <c>Win32</c>
+        /// </summary>
+        Win32 = 4
     }
 }
