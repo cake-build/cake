d0cc7e7f77bdd3e9899880e22c8572b29c44cf7d
Kristian Hellang <kristian@hellang.com>
2015-12-30 12:20:51 +0100

    Move Cake.Common towards a more .NET Core-friendly surface area

--- src/Cake.Common/Tools/MSBuild/MSBuildRunner.cs
+++ src/Cake.Common/Tools/MSBuild/MSBuildRunner.cs
@@ -113,7 +113,7 @@ namespace Cake.Common.Tools.MSBuild
                 case PlatformTarget.Win32:
                     return "Win32";
                 default:
-                    throw new InvalidEnumArgumentException("platform", (int)platform, typeof(PlatformTarget));
+                    throw new ArgumentOutOfRangeException("platform", platform, "Invalid platform");
             }
         }
 
