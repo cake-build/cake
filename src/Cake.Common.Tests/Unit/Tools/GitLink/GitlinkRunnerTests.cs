c95662d7da22df2834be2ac992e770d628d8f119
Gary Ewan Park <gep13@gep13.co.uk>
2016-01-10 17:38:36 +0000

    (GH-619) Added GitLink Support with Tests

---
+++ src/Cake.Common.Tests/Unit/Tools/GitLink/GitlinkRunnerTests.cs
@@ -0,0 +1,240 @@
+﻿using Cake.Common.Tests.Fixtures.Tools;
+using Cake.Core;
+using Cake.Core.IO;
+using Cake.Testing;
+using Cake.Testing.Xunit;
+using Xunit;
+
+namespace Cake.Common.Tests.Unit.Tools.GitLink
+{
+    public sealed class GitlinkRunnerTests
+    {
+        public sealed class TheRunMethod
+        {
+            [Fact]
+            public void Should_Throw_If_Repository_Root_Path_Is_Null()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.RepositoryRootPath = null;
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                Assert.IsArgumentNullException(result, "repositoryRootPath");
+            }
+
+            [Fact]
+            public void Should_Find_GitLink_Runner()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("/Working/tools/gitlink.exe", result.Path.FullPath);
+            }
+
+            [Fact]
+            public void Should_Throw_If_Process_Was_Not_Started()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.GivenProcessCannotStart();
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                Assert.IsType<CakeException>(result);
+                Assert.Equal("GitLink: Process was not started.", result.Message);
+            }
+
+            [Fact]
+            public void Should_Throw_If_Has_A_Non_Zero_Exit_Code()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.GivenProcessExitsWithCode(1);
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                Assert.IsType<CakeException>(result);
+                Assert.Equal("GitLink: Process returned an error.", result.Message);
+            }
+
+            [Fact]
+            public void Should_Use_Provided_Repository_Root_Path_In_Process_Arguments()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.RepositoryRootPath = "source";
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("\"/Working/source\"", result.Args);
+            }
+
+            [WindowsFact]
+            public void Should_Set_RepositoryUrl()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.Settings.RepositoryUrl = "http://mydomain.com";
+
+                // Then
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("\"c:/temp\" -u \"http://mydomain.com\"", result.Args);
+            }
+
+            [WindowsFact]
+            public void Should_Set_SolutionFileName()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.Settings.SolutionFileName = "solution.sln";
+
+                // Then
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("\"c:/temp\" -f \"solution.sln\"", result.Args);
+            }
+
+            [WindowsFact]
+            public void Should_Set_Configuration()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.Settings.Configuration = "Release";
+
+                // Then
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("\"c:/temp\" -c \"Release\"", result.Args);
+            }
+
+            [WindowsFact]
+            public void Should_Set_Platform()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.Settings.Platform = "AnyCPU";
+
+                // Then
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("\"c:/temp\" -p \"AnyCPU\"", result.Args);
+            }
+
+            [WindowsFact]
+            public void Should_Set_Branch()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.Settings.Branch = "master";
+
+                // Then
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("\"c:/temp\" -b \"master\"", result.Args);
+            }
+
+            [WindowsFact]
+            public void Should_Set_LogFilePath()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.Settings.LogFilePath = @"/temp/log.txt";
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("\"c:/temp\" -l \"/temp/log.txt\"", result.Args);
+            }
+
+            [WindowsFact]
+            public void Should_Set_ShaHash()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.Settings.ShaHash = "abcdef";
+
+                // Then
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("\"c:/temp\" -s \"abcdef\"", result.Args);
+            }
+
+            [WindowsFact]
+            public void Should_Set_PdbDirectory()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.Settings.PdbDirectoryPath = DirectoryPath.FromString("pdb/");
+
+                // Then
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("\"c:/temp\" -d \"/Working/pdb\"", result.Args);
+            }
+
+            [WindowsFact]
+            public void Should_Set_PowerShell_Switch()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.Settings.UsePowerShell = true;
+
+                // Then
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("\"c:/temp\" -powershell", result.Args);
+            }
+
+            [WindowsFact]
+            public void Should_Set_SkipVerify_Switch()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.Settings.SkipVerify = true;
+
+                // Then
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("\"c:/temp\" -skipverify", result.Args);
+            }
+
+            [WindowsFact]
+            public void Should_Set_IsDebug_Switch()
+            {
+                // Given
+                var fixture = new GitLinkFixture();
+                fixture.Settings.IsDebug = true;
+
+                // Then
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("\"c:/temp\" -debug", result.Args);
+            }
+        }
+    }
+}
---
+++ src/Cake.Common/Tools/GitLink/GitLinkAliases.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+
+namespace Cake.Common.Tools.GitLink
+{
+    /// <summary>
+    /// Contains functionality for working with GitLink.
+    /// </summary>
+    [CakeAliasCategory("GitTools")]
+    public static class GitLinkAliases
+    {
+        /// <summary>
+        /// Update pdb files to link all sources.
+        /// This will allow anyone to step through the source code while debugging without a symbol source server.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="repositoryRootPath">The Solution File to analyze.</param>
+        /// <example>
+        /// <code>
+        /// GitLink("C:/temp/solution");
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("GitLink")]
+        public static void GitLink(this ICakeContext context, DirectoryPath repositoryRootPath)
+        {
+            GitLink(context, repositoryRootPath, new GitLinkSettings());
+        }
+
+        /// <summary>
+        /// Update pdb files to link all sources, using specified settings.
+        /// This will allow anyone to step through the source code while debugging without a symbol source server.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="repositoryRootPath">The path to the Root of the Repository to analyze.</param>
+        /// <param name="settings">The settings.</param>
+        /// <example>
+        /// <code>
+        /// GitLink("C:/temp/solution", new GitLinkSettings {
+        ///     RepositoryUrl = new Uri("http://mydomain.com"),
+        ///     Branch        = "master",
+        ///     ShaHash       = "abcdef",
+        /// });
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("GitLink")]
+        public static void GitLink(this ICakeContext context, DirectoryPath repositoryRootPath, GitLinkSettings settings)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var runner = new GitLinkRunner(context.FileSystem, context.Environment, context.ProcessRunner, context.Globber, context.Log);
+            runner.Run(repositoryRootPath, settings);
+        }
+    }
+}
