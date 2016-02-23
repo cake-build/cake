f4eddf7ab55181af23f3035fe87008f2b539e6a8
Gary Ewan Park <gep13@gep13.co.uk>
2016-02-23 12:38:06 +0000

    (build) Bumped Version Number

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Cake")]
-[assembly: AssemblyVersion("0.8.0")]
-[assembly: AssemblyFileVersion("0.8.0")]
-[assembly: AssemblyInformationalVersion("0.8.0")]
+[assembly: AssemblyVersion("0.9.0")]
+[assembly: AssemblyFileVersion("0.9.0")]
+[assembly: AssemblyInformationalVersion("0.9.0")]
 [assembly: AssemblyCopyright("Copyright (c) Patrik Svensson, Mattias Karlsson, Gary Ewan Park and contributors")]
 
