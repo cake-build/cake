4af05fc6247809f905a6be2ffa2cc09e02befa93
Rodney Littles II <rodney.littles@gmail.com>
2016-03-03 23:54:14 -0600

    Added WiX heat support

---
+++ src/Cake.Common/Tools/WiX/Heat/WiXGenerateType.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+namespace Cake.Common.Tools.WiX.Heat
+{
+    /// <summary>
+    /// Type of elements to generate
+    /// </summary>
+    public enum WiXGenerateType
+    {
+        /// <summary>
+        /// Generates components
+        /// </summary>
+        Components,
+
+        /// <summary>
+        /// Generates a container
+        /// </summary>
+        Container,
+
+        /// <summary>
+        /// Generates a payload group
+        /// </summary>
+        PayloadGroup,
+
+        /// <summary>
+        /// Generates a layout
+        /// </summary>
+        Layout
+    }
+}
---
+++ src/Cake.Common/Tools/WiX/Heat/WiXTemplateType.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+namespace Cake.Common.Tools.WiX.Heat
+{
+    /// <summary>
+    /// Template type to use for harvesting.
+    /// </summary>
+    public enum WiXTemplateType
+    {
+        /// <summary>
+        /// TemplateType: <c>Fragment</c>
+        /// </summary>
+        Fragment = 0,
+
+        /// <summary>
+        /// TemplateType: <c>Module</c>
+        /// </summary>
+        Module = 1,
+
+        /// <summary>
+        /// TemplateType: <c>Product</c>
+        /// </summary>
+        Product = 2
+    }
+}
