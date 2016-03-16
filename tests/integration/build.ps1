36a2013968fff5d0efa4ef940207ca6747bebdda
Patrik Svensson <patrik@patriksvensson.se>
2016-03-16 23:54:07 +0100

    Cake is now being built.

--- tests/integration/build.ps1
+++ tests/integration/build.ps1
@@ -1,61 +1,49 @@
+[CmdletBinding()]
+Param(
+    [switch]$SkipBuildingCake
+)
+
 $PSScriptRoot = split-path -parent $MyInvocation.MyCommand.Definition;
 
-$TOOLS_DIR = Join-Path $PSScriptRoot "tools";
-$NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe";
-$NUGET_URL = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
+# Load utilities.
+. (Join-Path $PSScriptRoot "utilities.ps1");
 
-#################################################################################
-
-Function CleanDirectory([string]$Directory) {
-    if(Test-Path $Directory) {
-        Remove-Item -Path $Directory -Recurse -Force
-    }
-    New-Item -Path $Directory -Type directory | out-null;
+# Make sure the tools directory exist.
+$ToolsPath = Join-Path $PSScriptRoot "tools";
+if(!(Test-Path $ToolsPath)) {
+    Write-Verbose "Creating tools directory...";
+    New-Item -Path $ToolsPath -Type directory | out-null;
 }
 
-Function GetCakeDirectory() {
-    Push-Location
-    Set-Location (Join-Path $PSScriptRoot "../..");
-    $OriginalCakePath = (Get-ChildItem -Path "./build" -Filter "Cake.exe" -Recurse | Select -First 1).Directory.FullName;
-    Pop-Location
-    Return $OriginalCakePath;
-}
-
-#################################################################################
-
-# Tools directory
-if(!(Test-Path $TOOLS_DIR)) {
-    Write-Verbose -Message "Creating tools directory...";
-    New-Item -Path $TOOLS_DIR -Type directory | out-null;
-}
-
-# NuGet
-if (!(Test-Path $NUGET_EXE)) {
-    (New-Object System.Net.WebClient).DownloadFile($NUGET_URL, $NUGET_EXE);
-    if (!(Test-Path $NUGET_EXE)) {
+# Make sure NuGet exist.
+$NuGetPath = Join-Path $ToolsPath "nuget.exe";
+$NuGetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
+if (!(Test-Path $NuGetPath)) {
+    Write-Verbose "Downloading NuGet..."
+    (New-Object System.Net.WebClient).DownloadFile($NuGetUrl, $NuGetPath);
+    if (!(Test-Path $NuGetPath)) {
         Throw "Could not find NuGet.exe";
     }
 }
 
 # Build Cake.
-$RootDirectory = (Join-Path $PSScriptRoot "../..");
-CleanDirectory((Join-Path $RootDirectory "build"))
-Push-Location;
-Set-Location $RootDirectory;
-Write-Output "Building Cake...";
-Invoke-Expression "./build.ps1 -Target Copy-Files" | Out-Null;
-if($LASTEXITCODE -ne 0) {
-    Throw "An error occured while building Cake.";
+if(!$SkipBuildingCake.IsPresent) {
+    Write-Host "Building Cake..."
+    BuildCake($PSScriptRoot);
 }
-Pop-Location
 
-# Clean directory
-CleanDirectory((Join-Path $TOOLS_DIR "Cake"));
-$CakePath = GetCakeDirectory; 
-Write-Host "Cake Directory: $CakePath";
+# Get the built Cake path.
+$BuiltCakePath = GetCakePath($PSScriptRoot);
+if([string]::IsNullOrWhiteSpace($BuiltCakePath)) {
+    Throw "Could not resolve built Cake path."
+}
 
-# Copy files
+# Clean the local Cake path.
+$CakePath = (Join-Path $ToolsPath "Cake");
+CleanDirectory($CakePath);
+Copy-Item "$BuiltCakePath/*.*" $CakePath -Recurse
 
 # Run tests using new Cake.
-Write-Output "Running integration tests using new Cake.";
-
+Write-Output "Running integration tests using new Cake...";
+$CakeExePath = Join-Path $CakePath "Cake.exe";
+&$CakeExePath "--version"
