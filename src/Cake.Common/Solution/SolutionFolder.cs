f75c8b9e00e41ff787b9c419d8022fa290158f1d
endrton <peto.gyorgy@gmail.com>
2016-05-30 09:47:34 +0200

    New Feature: Solution folders are parsed from sln file. Unit tests are added.

---
+++ src/Cake.Common/Solution/SolutionFolder.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Cake.Core.IO;
+
+namespace Cake.Common.Solution
+{
+    /// <summary>
+    /// Represents a folder in a MSBuild solution.
+    /// </summary>
+    public sealed class SolutionFolder : SolutionProject
+    {
+        /// <summary>
+        /// Visual Studio project type guid for solution folder
+        /// </summary>
+        /// <remarks>
+        /// http://www.codeproject.com/Reference/720512/List-of-Visual-Studio-Project-Type-GUIDs
+        /// </remarks>
+        public const string TypeIdentifier = "{2150E333-8FDC-42A3-9474-1A3956D46DE8}";
+
+        private readonly List<SolutionProject> _items;
+
+        /// <summary>
+        /// Child items of this folder
+        /// </summary>
+        public List<SolutionProject> Items
+        {
+            get { return _items; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SolutionFolder"/> class.
+        /// </summary>
+        /// <param name="id">The folder project identity.</param>
+        /// <param name="name">The folder name.</param>
+        /// <param name="path">The folder path.</param>
+        public SolutionFolder(string id, string name, FilePath path) : base(id, name, path, TypeIdentifier)
+        {
+            _items = new List<SolutionProject>();
+        }
+    }
+}
