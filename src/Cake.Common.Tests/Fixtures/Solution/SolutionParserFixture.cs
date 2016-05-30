23e44cd89788124c992591fe51e87a9f5d25454c
endrton <peto.gyorgy@gmail.com>
2016-05-30 09:47:34 +0200

    New Feature: Solution folders are parsed from sln file. Unit tests are added.

---
+++ src/Cake.Common.Tests/Fixtures/Solution/SolutionParserFixture.cs
@@ -0,0 +1,24 @@
+﻿using Cake.Core;
+using Cake.Core.IO;
+using Cake.Testing;
+using NSubstitute;
+
+namespace Cake.Common.Tests.Fixtures.Solution
+{
+    internal sealed class SolutionParserFixture
+    {
+        public FakeEnvironment Environment { get; set; }
+        public FakeFileSystem FileSystem { get; set; }
+        public SolutionParserFixture()
+        {
+            Environment = FakeEnvironment.CreateWindowsEnvironment();
+            FileSystem = new FakeFileSystem(Environment);
+        }
+
+        public FilePath WithSolutionFile(string slnContent)
+        {
+            var file = FileSystem.CreateFile("/Working/dummySolution.sln").SetContent(slnContent);
+            return file.Path;
+        }
+    }
+}
---
+++ src/Cake.Common.Tests/Unit/Solution/SolutionParserAliasesTests.cs
@@ -0,0 +1,38 @@
+﻿using Cake.Common.Solution;
+using Cake.Core;
+using Cake.Core.IO;
+using NSubstitute;
+using Xunit;
+
+namespace Cake.Common.Tests.Unit.Solution
+{
+    public sealed class SolutionParserAliasesTests
+    {
+        public sealed class TheParseSolutionMethod
+        {
+            [Fact]
+            public void Should_Throw_If_Context_Is_Null()
+            {
+                // Given, When
+                var result = Record.Exception(() => SolutionAliases.ParseSolution(null, null));
+                // Then
+                Assert.IsArgumentNullException(result, "context");
+            }
+
+            [Fact]
+            public void Should_Throw_If_SolutionPath_Is_Null()
+            {
+                // Given
+                var cakeContext = Substitute.For<ICakeContext>();
+                var cakeEnvironment = Substitute.For<ICakeEnvironment>();
+                var fileSystem = Substitute.For<IFileSystem>();
+                cakeContext.Environment.Returns(cakeEnvironment);
+                cakeContext.FileSystem.Returns(fileSystem);
+                // When
+                var result = Record.Exception(() => SolutionAliases.ParseSolution(cakeContext, null));
+                // Then
+                Assert.IsArgumentNullException(result, "solutionPath");
+            }
+        }
+    }
+}
