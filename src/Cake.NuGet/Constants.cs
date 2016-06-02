b5a8413ecce62de29e3f846bb5e4602f00174642
Scott Mackay <scott.mackay@maclean.co.uk>
2016-06-02 17:43:17 +0100

    (GH-960) Add configuration for default NuGet source

---
+++ src/Cake.NuGet/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Cake.NuGet
+{
+    internal static class Constants
+    {
+        public static class NuGet
+        {
+            /// <summary>
+            /// The config key name for overriding the default nuget package source
+            /// </summary>
+            public const string Source = "NuGet_Source";
+        }
+    }
+}
