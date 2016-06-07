c2a57ab6cdd658ec983937b92a5d6dfdd044714f
Gary Ewan Park <gep13@gep13.co.uk>
2016-06-07 21:03:42 +0100

    Updated version and release notes.

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Cake")]
-[assembly: AssemblyVersion("0.12.0.0")]
-[assembly: AssemblyFileVersion("0.12.0.0")]
-[assembly: AssemblyInformationalVersion("0.12.0-beta.1+0.Branch.release/0.12.0.Sha.db292d321378109991d7dbb9c78aec6dd7b7ebf5")]
+[assembly: AssemblyVersion("0.13.0.0")]
+[assembly: AssemblyFileVersion("0.13.0.0")]
+[assembly: AssemblyInformationalVersion("0.13.0-beta.1+0.Branch.release/0.13.0.Sha.cb8b8acaee3dd90cb1a0e21fc079b4e65690750d")]
 [assembly: AssemblyCopyright("Copyright (c) Patrik Svensson, Mattias Karlsson, Gary Ewan Park and contributors")]
 
