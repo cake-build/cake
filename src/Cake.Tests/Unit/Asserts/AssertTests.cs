5c8c05fa6a2481abfe6d69e99286f37803e40730
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2016-07-14 19:50:05 +0100

    Added Assert.NotNull() overload accepting user message
    Added `Assert.NotNull()` overload accepting user message to make debugging failing assertions easier. Also added tests for `Assert.NotNull()`.

---
+++ src/Cake.Tests/Unit/Asserts/AssertTests.cs
@@ -0,0 +1,61 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+using Xunit.Sdk;
+
+namespace Cake.Tests.Unit.Asserts
+{
+    public sealed class AssertTests
+    {
+        public sealed class NotNull
+        {
+            [Fact]
+            public void Should_Not_Throw_If_Object_Is_Not_Null()
+            {
+                // Given
+                var @object = new object();
+
+                // When, Then
+                Assert.NotNull(@object);
+            }
+
+            [Fact]
+            public void Should_Not_Throw_With_User_Message_If_Object_Is_Not_Null()
+            {
+                // Given
+                var @object = new object();
+
+                // When, Then
+                Assert.NotNull(@object, "User Message");
+            }
+
+            [Fact]
+            public void Should_Throw_If_Object_Is_Null()
+            {
+                // Given
+                object @object = null;
+
+                // When
+                var exception = Assert.Throws<NotNullException>(() => Assert.NotNull(@object));
+
+                // Then
+                Assert.Equal("Assert.NotNull() Failure.", exception.Message);
+            }
+
+            [Fact]
+            public void Should_Throw_With_User_Message_If_Object_Is_Null()
+            {
+                // Given
+                object @object = null;
+
+                // When
+                var exception = Assert.Throws<NotNullException>(() => Assert.NotNull(@object, "User Message"));
+
+                // Then
+                Assert.Equal("User Message. Assert.NotNull() Failure.", exception.Message);
+            }
+        }
+    }
+}
