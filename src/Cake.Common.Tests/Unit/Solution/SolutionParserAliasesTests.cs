f95878b5ad8424872e87e5dc113902f64c67f78e
Gary Ewan Park <gep13@gep13.co.uk>
2016-07-25 19:37:07 +0100

    (GH-1118) Corrected spacing around header and end of file

--- src/Cake.Common.Tests/Unit/Solution/SolutionParserAliasesTests.cs
+++ src/Cake.Common.Tests/Unit/Solution/SolutionParserAliasesTests.cs
@@ -1,4 +1,8 @@
-﻿using Cake.Common.Solution;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Cake.Common.Solution;
 using Cake.Core;
 using Cake.Core.IO;
 using NSubstitute;
