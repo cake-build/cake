ff025d1dfca6437f803c83449dcb7219f616a587
Jeremie Desautels <desautelsj@hotmail.com>
2016-08-09 15:22:36 -0500

    Allow custom loggers when using VSTest
    Allow custom loggers when using VSTest
    
    Fix SA1122 : CSharp.Readability : Use string.Empty rather than ""
    
    Fix Warning SA1208 Using directive for 'System' must appear before directive for 'Cake.Common.Tools.VSTest'
    
    StringComparison.InvariantCultureIgnoreCase is no longer available in .NETStandard Version 1.6 Therefore replacing it with StringComparison.OrdinalIgnoreCase
    
    Fix typo in comment
    
    Change the 'Logger' property to a string
    
    Get rid of Warning RECS0072 'base.' is redundant and can safely be removed

--- src/Cake.Common/Tools/VSTest/VSTestSettings.cs
+++ src/Cake.Common/Tools/VSTest/VSTestSettings.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using Cake.Core.IO;
 using Cake.Core.Tooling;
 
@@ -38,8 +39,12 @@ namespace Cake.Common.Tools.VSTest
         public VSTestFrameworkVersion FrameworkVersion { get; set; }
 
         /// <summary>
-        /// Gets or sets the logger to use for test results.
+        /// Gets or sets the name of your logger. Possible values:
+        /// - A blank string (or null): no logger
+        /// - "trx": Visual Studio's built-in logger
+        /// - "AppVeyor": AppVeyor's custom logger which is available only when building your solution on the AppVeyor platform
+        /// - any custom value: the name of your custom logger
         /// </summary>
-        public VSTestLogger Logger { get; set; }
+        public string Logger { get; set; }
     }
 }
