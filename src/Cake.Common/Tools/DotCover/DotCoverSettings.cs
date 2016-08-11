8e7b783c10f5227b954d8b16fa9cab1bf1835489
Martin Björkström <martin.bjorkstrom@gmail.com>
2016-08-11 23:33:21 +0300

    Add DotCover Report and refactor
    Solves #724

--- src/Cake.Common/Tools/DotCover/DotCoverSettings.cs
+++ src/Cake.Common/Tools/DotCover/DotCoverSettings.cs
@@ -1,10 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-using System;
-using System.Collections.Generic;
-using Cake.Core.IO;
+﻿using Cake.Core.IO;
 using Cake.Core.Tooling;
 
 namespace Cake.Common.Tools.DotCover
@@ -14,58 +8,10 @@ namespace Cake.Common.Tools.DotCover
     /// </summary>
     public abstract class DotCoverSettings : ToolSettings
     {
-        private readonly HashSet<string> _scope;
-        private readonly HashSet<string> _filters;
-        private readonly HashSet<string> _attributeFilters;
-
-        /// <summary>
-        /// Gets or sets program working directory
-        /// This represents the <c>/TargetWorkingDir</c> option.
-        /// </summary>
-        public DirectoryPath TargetWorkingDir { get; set; }
-
-        /// <summary>
-        /// Gets the assemblies loaded in the specified scope into coverage results.
-        /// Ant-style patterns are supported here (e.g.ProjectFolder/**/*.dll)
-        /// This represents the <c>/Scope</c> option.
-        /// </summary>
-        public ISet<string> Scope => _scope;
-
-        /// <summary>
-        /// Gets the coverage filters using the following syntax: +:module=*;class=*;function=*;
-        /// Use -:myassembly to exclude an assembly from code coverage.
-        /// Asterisk wildcard (*) is supported here.
-        /// This represents the <c>/Filters</c> option.
-        /// </summary>
-        public ISet<string> Filters => _filters;
-
-        /// <summary>
-        /// Gets the attribute filters using the following syntax: filter1;filter2;...
-        /// Asterisk wildcard(*) is supported here
-        /// This represents the <c>/AttributeFilters</c> option.
-        /// </summary>
-        public ISet<string> AttributeFilters => _attributeFilters;
-
-        /// <summary>
-        /// Gets or sets a value indicating whether the default (automatically added) filters should be disabled
-        /// This represents the <c>/DisableDefaultFilters</c> option.
-        /// </summary>
-        public bool DisableDefaultFilters { get; set; }
-
         /// <summary>
         /// Gets or sets a value that enables logging and specifies log file name
         /// This represents the <c>/LogFile</c> option.
         /// </summary>
         public FilePath LogFile { get; set; }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="DotCoverSettings"/> class.
-        /// </summary>
-        protected DotCoverSettings()
-        {
-            _scope = new HashSet<string>(StringComparer.Ordinal);
-            _filters = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
-            _attributeFilters = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
-        }
     }
 }
