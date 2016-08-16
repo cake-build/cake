d072ee87bb7a71af12307e8d98e7efc8c7d3e365
Jeremie Desautels <desautelsj@hotmail.com>
2016-08-16 13:37:24 -0500

    Fix Warning SA1208 Using directive for 'System' must appear before directive for 'Cake.Common.Tools.VSTest'

--- src/Cake.Common.Tests/Unit/Tools/VSTest/VSTestSettingsTests.cs
+++ src/Cake.Common.Tests/Unit/Tools/VSTest/VSTestSettingsTests.cs
@@ -2,8 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Cake.Common.Tools.VSTest;
 using System;
+using Cake.Common.Tools.VSTest;
 using Xunit;
 
 namespace Cake.Common.Tests.Unit.Tools.VSTest
