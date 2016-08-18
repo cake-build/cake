17b3bb8a77e69a6187a0c900e67dff65c0adbeeb
kcamp <campkd@gmail.com>
2016-08-18 10:09:38 -0400

    GH-1166: refactor the MethodAliasGenerator to support optional parameters in alias methods
    Refactor the parameter concerns in the method alias tokenizer to support
    optional params, extract logic to ParameterEmitter class, add supporting tests.
    
    Optional params have to be compile-time constants, so this keeps the surface area reasonable.
    Nullable<> parameters can be optional, but the default value, if not null,
    has to be cast as the inner type to be correct.
    
    Add supporting tests for the various use cases.

---
+++ src/Cake.Core.Tests/Unit/Scripting/CodeGen/ParameterEmitterTests.cs
@@ -0,0 +1,440 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Linq;
+using System.Reflection;
+using Cake.Core.Scripting.CodeGen;
+using Xunit;
+// ReSharper disable UnusedMember.Local
+// ReSharper disable UnusedParameter.Local
+namespace Cake.Core.Tests.Unit.Scripting.CodeGen
+{
+    public sealed class ParameterEmitterTests
+    {
+        private enum TestEnum
+        {
+            None,
+            Some,
+            All
+        }
+
+        private static class ParameterFixture
+        {
+            public static void RequiredInt(int arg)
+            {
+            }
+
+            public static void RequiredDouble(double arg)
+            {
+            }
+
+            public static void RequiredDecimal(decimal arg)
+            {
+            }
+
+            public static void RequiredLong(long arg)
+            {
+            }
+
+            public static void RequiredShort(short arg)
+            {
+            }
+
+            public static void RequiredSByte(sbyte arg)
+            {
+            }
+
+            public static void RequiredByte(byte arg)
+            {
+            }
+
+            public static void RequiredULong(ulong arg)
+            {
+            }
+
+            public static void RequiredUShort(ushort arg)
+            {
+            }
+
+            public static void RequiredUInt(uint arg)
+            {
+            }
+
+            public static void RequiredFloat(float arg)
+            {
+            }
+
+            public static void RequiredEnum(TestEnum arg)
+            {
+            }
+
+            public static void RequiredBool(bool arg)
+            {
+            }
+
+            public static void RequiredString(string arg)
+            {
+            }
+
+            public static void RequiredObject(object arg)
+            {
+            }
+
+            public static void RequiredDateTime(DateTime arg)
+            {
+            }
+
+            public static void RequiredInterface(IDisposable arg)
+            {
+            }
+
+            public static void RequiredClass(CakeContext arg)
+            {
+            }
+
+            public static void RequiredType(Type type)
+            {
+            }
+
+            public static void RequiredGenericEnumerable(System.Collections.Generic.IEnumerable<string> items)
+            {
+            }
+
+            public static void OptionalInt(int arg = 1)
+            {
+            }
+
+            public static void OptionalDouble(double arg = 1)
+            {
+            }
+
+            public static void OptionalDecimal(decimal arg = 1)
+            {
+            }
+
+            public static void OptionalLong(long arg = 1)
+            {
+            }
+
+            public static void OptionalShort(short arg = 1)
+            {
+            }
+
+            public static void OptionalSByte(sbyte arg = 1)
+            {
+            }
+
+            public static void OptionalByte(byte arg = 1)
+            {
+            }
+
+            public static void OptionalULong(ulong arg = 1)
+            {
+            }
+
+            public static void OptionalUShort(ushort arg = 1)
+            {
+            }
+
+            public static void OptionalUInt(uint arg = 1)
+            {
+            }
+
+            public static void OptionalFloat(float arg = 1)
+            {
+            }
+
+            public static void OptionalEnum(TestEnum arg = TestEnum.All)
+            {
+            }
+
+            public static void OptionalBool(bool arg = true)
+            {
+            }
+
+            public static void OptionalString(string arg = "value")
+            {
+            }
+
+            public static void OptionalObject(object arg = null)
+            {
+            }
+
+            public static void OptionalInterface(IDisposable arg = null)
+            {
+            }
+
+            public static void OptionalClass(CakeContext arg = null)
+            {
+            }
+
+            public static void OptionalType(Type type = null)
+            {
+            }
+
+            public static void OptionalGenericEnumerable(System.Collections.Generic.IEnumerable<string> items = null)
+            {
+            }
+
+            public static void RequiredNullableInt(int? arg)
+            {
+            }
+
+            public static void RequiredNullableDouble(double? arg)
+            {
+            }
+
+            public static void RequiredNullableDecimal(decimal? arg)
+            {
+            }
+
+            public static void RequiredNullableLong(long? arg)
+            {
+            }
+
+            public static void RequiredNullableShort(short? arg)
+            {
+            }
+
+            public static void RequiredNullableSByte(sbyte? arg)
+            {
+            }
+
+            public static void RequiredNullableByte(byte? arg)
+            {
+            }
+
+            public static void RequiredNullableULong(ulong? arg)
+            {
+            }
+
+            public static void RequiredNullableUShort(ushort? arg)
+            {
+            }
+
+            public static void RequiredNullableUInt(uint? arg)
+            {
+            }
+
+            public static void RequiredNullableFloat(float? arg)
+            {
+            }
+
+            public static void RequiredNullableEnum(TestEnum? arg)
+            {
+            }
+
+            public static void RequiredNullableBool(bool? arg)
+            {
+            }
+
+            public static void RequiredNullableDateTime(DateTime? arg)
+            {
+            }
+
+            public static void OptionalNullableIntWithNullDefault(int? arg = null)
+            {
+            }
+
+            public static void OptionalNullableDoubleWithNullDefault(double? arg = null)
+            {
+            }
+
+            public static void OptionalNullableDecimalWithNullDefault(decimal? arg = null)
+            {
+            }
+
+            public static void OptionalNullableLongWithNullDefault(long? arg = null)
+            {
+            }
+
+            public static void OptionalNullableShortWithNullDefault(short? arg = null)
+            {
+            }
+
+            public static void OptionalNullableSByteWithNullDefault(sbyte? arg = null)
+            {
+            }
+
+            public static void OptionalNullableByteWithNullDefault(byte? arg = null)
+            {
+            }
+
+            public static void OptionalNullableULongWithNullDefault(ulong? arg = null)
+            {
+            }
+
+            public static void OptionalNullableUShortWithNullDefault(ushort? arg = null)
+            {
+            }
+
+            public static void OptionalNullableUIntWithNullDefault(uint? arg = null)
+            {
+            }
+
+            public static void OptionalNullableFloatWithNullDefault(float? arg = null)
+            {
+            }
+
+            public static void OptionalNullableEnumWithNullDefault(TestEnum? arg = null)
+            {
+            }
+
+            public static void OptionalNullableBoolWithNullDefault(bool? arg = null)
+            {
+            }
+
+            public static void OptionalNullableDateTimeWithNullDefault(DateTime? arg = null)
+            {
+            }
+
+            public static void OptionalNullableIntWithNonNullDefault(int? arg = 1)
+            {
+            }
+
+            public static void OptionalNullableDoubleWithNonNullDefault(double? arg = 1)
+            {
+            }
+
+            public static void OptionalNullableDecimalWithNonNullDefault(decimal? arg = 1)
+            {
+            }
+
+            public static void OptionalNullableLongWithNonNullDefault(long? arg = 1)
+            {
+            }
+
+            public static void OptionalNullableShortWithNonNullDefault(short? arg = 1)
+            {
+            }
+
+            public static void OptionalNullableSByteWithNonNullDefault(sbyte? arg = 1)
+            {
+            }
+
+            public static void OptionalNullableByteWithNonNullDefault(byte? arg = 1)
+            {
+            }
+
+            public static void OptionalNullableULongWithNonNullDefault(ulong? arg = 1)
+            {
+            }
+
+            public static void OptionalNullableUShortWithNonNullDefault(ushort? arg = 1)
+            {
+            }
+
+            public static void OptionalNullableUIntWithNonNullDefault(uint? arg = 1)
+            {
+            }
+
+            public static void OptionalNullableFloatWithNonNullDefault(float? arg = 1)
+            {
+            }
+
+            public static void OptionalNullableEnumWithNonNullDefault(TestEnum? arg = TestEnum.Some)
+            {
+            }
+
+            public static void OptionalNullableBoolWithNonNullDefault(bool? arg = true)
+            {
+            }
+        }
+
+        [Theory]
+        [InlineData("RequiredInt", "System.Int32 arg")]
+        [InlineData("RequiredDouble", "System.Double arg")]
+        [InlineData("RequiredDecimal", "System.Decimal arg")]
+        [InlineData("RequiredLong", "System.Int64 arg")]
+        [InlineData("RequiredShort", "System.Int16 arg")]
+        [InlineData("RequiredSByte", "System.SByte arg")]
+        [InlineData("RequiredByte", "System.Byte arg")]
+        [InlineData("RequiredULong", "System.UInt64 arg")]
+        [InlineData("RequiredUShort", "System.UInt16 arg")]
+        [InlineData("RequiredUInt", "System.UInt32 arg")]
+        [InlineData("RequiredFloat", "System.Single arg")]
+        [InlineData("RequiredEnum", "Cake.Core.Tests.Unit.Scripting.CodeGen.ParameterEmitterTests+TestEnum arg")]
+        [InlineData("RequiredBool", "System.Boolean arg")]
+        [InlineData("RequiredString", "System.String arg")]
+        [InlineData("RequiredObject", "System.Object arg")]
+        [InlineData("RequiredDateTime", "System.DateTime arg")]
+        [InlineData("RequiredInterface", "System.IDisposable arg")]
+        [InlineData("RequiredClass", "Cake.Core.CakeContext arg")]
+        [InlineData("RequiredType", "System.Type type")]
+        [InlineData("RequiredGenericEnumerable", "System.Collections.Generic.IEnumerable<System.String> items")]
+        [InlineData("OptionalInt", "System.Int32 arg = (System.Int32)1")]
+        [InlineData("OptionalDouble", "System.Double arg = (System.Double)1")]
+        [InlineData("OptionalDecimal", "System.Decimal arg = (System.Decimal)1")]
+        [InlineData("OptionalLong", "System.Int64 arg = (System.Int64)1")]
+        [InlineData("OptionalShort", "System.Int16 arg = (System.Int16)1")]
+        [InlineData("OptionalSByte", "System.SByte arg = (System.SByte)1")]
+        [InlineData("OptionalByte", "System.Byte arg = (System.Byte)1")]
+        [InlineData("OptionalULong", "System.UInt64 arg = (System.UInt64)1")]
+        [InlineData("OptionalUShort", "System.UInt16 arg = (System.UInt16)1")]
+        [InlineData("OptionalUInt", "System.UInt32 arg = (System.UInt32)1")]
+        [InlineData("OptionalFloat", "System.Single arg = (System.Single)1")]
+        [InlineData("OptionalEnum", "Cake.Core.Tests.Unit.Scripting.CodeGen.ParameterEmitterTests+TestEnum arg = (Cake.Core.Tests.Unit.Scripting.CodeGen.ParameterEmitterTests+TestEnum)2")]
+        [InlineData("OptionalBool", "System.Boolean arg = true")]
+        [InlineData("OptionalString", "System.String arg = \"value\"")]
+        [InlineData("OptionalObject", "System.Object arg = null")]
+        [InlineData("OptionalInterface", "System.IDisposable arg = null")]
+        [InlineData("OptionalClass", "Cake.Core.CakeContext arg = null")]
+        [InlineData("OptionalType", "System.Type type = null")]
+        [InlineData("OptionalGenericEnumerable", "System.Collections.Generic.IEnumerable<System.String> items = null")]
+        [InlineData("RequiredNullableInt", "System.Nullable<System.Int32> arg")]
+        [InlineData("RequiredNullableDouble", "System.Nullable<System.Double> arg")]
+        [InlineData("RequiredNullableDecimal", "System.Nullable<System.Decimal> arg")]
+        [InlineData("RequiredNullableLong", "System.Nullable<System.Int64> arg")]
+        [InlineData("RequiredNullableShort", "System.Nullable<System.Int16> arg")]
+        [InlineData("RequiredNullableSByte", "System.Nullable<System.SByte> arg")]
+        [InlineData("RequiredNullableByte", "System.Nullable<System.Byte> arg")]
+        [InlineData("RequiredNullableULong", "System.Nullable<System.UInt64> arg")]
+        [InlineData("RequiredNullableUShort", "System.Nullable<System.UInt16> arg")]
+        [InlineData("RequiredNullableUInt", "System.Nullable<System.UInt32> arg")]
+        [InlineData("RequiredNullableFloat", "System.Nullable<System.Single> arg")]
+        [InlineData("RequiredNullableEnum", "System.Nullable<Cake.Core.Tests.Unit.Scripting.CodeGen.ParameterEmitterTests+TestEnum> arg")]
+        [InlineData("RequiredNullableBool", "System.Nullable<System.Boolean> arg")]
+        [InlineData("RequiredNullableDateTime", "System.Nullable<System.DateTime> arg")]
+        [InlineData("OptionalNullableIntWithNullDefault", "System.Nullable<System.Int32> arg = null")]
+        [InlineData("OptionalNullableDoubleWithNullDefault", "System.Nullable<System.Double> arg = null")]
+        [InlineData("OptionalNullableDecimalWithNullDefault", "System.Nullable<System.Decimal> arg = null")]
+        [InlineData("OptionalNullableLongWithNullDefault", "System.Nullable<System.Int64> arg = null")]
+        [InlineData("OptionalNullableShortWithNullDefault", "System.Nullable<System.Int16> arg = null")]
+        [InlineData("OptionalNullableSByteWithNullDefault", "System.Nullable<System.SByte> arg = null")]
+        [InlineData("OptionalNullableByteWithNullDefault", "System.Nullable<System.Byte> arg = null")]
+        [InlineData("OptionalNullableULongWithNullDefault", "System.Nullable<System.UInt64> arg = null")]
+        [InlineData("OptionalNullableUShortWithNullDefault", "System.Nullable<System.UInt16> arg = null")]
+        [InlineData("OptionalNullableUIntWithNullDefault", "System.Nullable<System.UInt32> arg = null")]
+        [InlineData("OptionalNullableFloatWithNullDefault", "System.Nullable<System.Single> arg = null")]
+        [InlineData("OptionalNullableEnumWithNullDefault", "System.Nullable<Cake.Core.Tests.Unit.Scripting.CodeGen.ParameterEmitterTests+TestEnum> arg = null")]
+        [InlineData("OptionalNullableBoolWithNullDefault", "System.Nullable<System.Boolean> arg = null")]
+        [InlineData("OptionalNullableDateTimeWithNullDefault", "System.Nullable<System.DateTime> arg = null")]
+        [InlineData("OptionalNullableIntWithNonNullDefault", "System.Nullable<System.Int32> arg = (System.Int32)1")]
+        [InlineData("OptionalNullableDoubleWithNonNullDefault", "System.Nullable<System.Double> arg = (System.Double)1")]
+        [InlineData("OptionalNullableDecimalWithNonNullDefault", "System.Nullable<System.Decimal> arg = (System.Decimal)1")]
+        [InlineData("OptionalNullableLongWithNonNullDefault", "System.Nullable<System.Int64> arg = (System.Int64)1")]
+        [InlineData("OptionalNullableShortWithNonNullDefault", "System.Nullable<System.Int16> arg = (System.Int16)1")]
+        [InlineData("OptionalNullableSByteWithNonNullDefault", "System.Nullable<System.SByte> arg = (System.SByte)1")]
+        [InlineData("OptionalNullableByteWithNonNullDefault", "System.Nullable<System.Byte> arg = (System.Byte)1")]
+        [InlineData("OptionalNullableULongWithNonNullDefault", "System.Nullable<System.UInt64> arg = (System.UInt64)1")]
+        [InlineData("OptionalNullableUShortWithNonNullDefault", "System.Nullable<System.UInt16> arg = (System.UInt16)1")]
+        [InlineData("OptionalNullableUIntWithNonNullDefault", "System.Nullable<System.UInt32> arg = (System.UInt32)1")]
+        [InlineData("OptionalNullableFloatWithNonNullDefault", "System.Nullable<System.Single> arg = (System.Single)1")]
+        [InlineData("OptionalNullableEnumWithNonNullDefault", "System.Nullable<Cake.Core.Tests.Unit.Scripting.CodeGen.ParameterEmitterTests+TestEnum> arg = (Cake.Core.Tests.Unit.Scripting.CodeGen.ParameterEmitterTests+TestEnum)1")]
+        [InlineData("OptionalNullableBoolWithNonNullDefault", "System.Nullable<System.Boolean> arg = (System.Boolean)true")]
+        public void Should_Return_Correct_Generated_Code_For_Method_Parameters(string methodName, string expected)
+        {
+            // Given
+            var method = typeof(ParameterFixture).GetMethod(methodName, BindingFlags.Static | BindingFlags.Public);
+            var parameter = method.GetParameters().FirstOrDefault();
+
+            // When
+            var result = ParameterEmitter.Emit(parameter, true);
+
+            // Then
+            Assert.Equal(expected, result);
+        }
+    }
+}
