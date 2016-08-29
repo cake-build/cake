2ad6140b9ee0cbb6d9eaf4f610c8426df8ff6b28
Mattias Karlsson <matkar@wcom.se>
2016-08-29 14:17:16 +0200

    Adds integration tests for Cake.Common.Tools.DotNetCore.DotNetCoreAliases * releates to #1173

---
+++ tests/integration/resources/Cake.Common/Tools/DotNetCore/hwapp.common/Greeter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HWApp.Common
+{
+    public static class Greeter
+    {
+        public static string GetGreeting(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            return $"Hello {name}!";
+        }
+    }
+}
