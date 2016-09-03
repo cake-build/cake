26aff47a02df8af6218c879e03a100e45d0a1b44
Christian Palmstierna <christian.palmstierna@gmail.com>
2016-09-03 17:27:27 +0200

    Added OctoPack tool

---
+++ src/Cake.Common/Tools/OctopusDeploy/OctopusPackSettings.cs
@@ -0,0 +1,71 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using Cake.Core.IO;
+using Cake.Core.Tooling;
+
+namespace Cake.Common.Tools.OctopusDeploy
+{
+    /// <summary>
+    /// Contains the settings used by OctoPack.
+    /// </summary>
+    public sealed class OctopusPackSettings : ToolSettings
+    {
+        /// <summary>
+        /// Gets or sets the version.
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// Gets or sets the package format.
+        /// </summary>
+        public OctopusPackFormat Format { get; set; }
+
+        /// <summary>
+        /// Gets or sets the folder into which the package will be written. Defaults to the current folder.
+        /// </summary>
+        public DirectoryPath OutFolder { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root folder containing files and folders to pack. Defaults to the current folder.
+        /// </summary>
+        public DirectoryPath BasePath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the author. Only applies to NuGet packages.
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title. Only applies to NuGet packages.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description. Only applies to NuGet packages.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the release notes. Only applies to NuGet packages.
+        /// </summary>
+        public string ReleaseNotes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the release notes file. Only applies to NuGet packages.
+        /// </summary>
+        public FilePath ReleaseNotesFile { get; set; }
+
+        /// <summary>
+        /// Gets or sets the file patterns to include. If none are specified, defaults to **.
+        /// </summary>
+        public ICollection<string> Include { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to allow an existing package with the same ID/version to be overwriten.
+        /// </summary>
+        public bool Overwrite { get; set; }
+    }
+}
