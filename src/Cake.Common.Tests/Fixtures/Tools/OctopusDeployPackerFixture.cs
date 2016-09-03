089d546a3a6f0d93adde5fe71de39558cb17cb24
Christian Palmstierna <christian.palmstierna@gmail.com>
2016-09-03 17:27:27 +0200

    Added OctoPack tool

---
+++ src/Cake.Common.Tests/Fixtures/Tools/OctopusDeployPackerFixture.cs
@@ -0,0 +1,25 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Cake.Common.Tools.OctopusDeploy;
+using Cake.Testing.Fixtures;
+
+namespace Cake.Common.Tests.Fixtures.Tools
+{
+    internal sealed class OctopusDeployPackerFixture : ToolFixture<OctopusPackSettings>
+    {
+        public string Id { get; set; }
+
+        public OctopusDeployPackerFixture()
+            : base("octo.exe")
+        {
+        }
+
+        protected override void RunTool()
+        {
+            var tool = new OctopusDeployPacker(FileSystem, Environment, ProcessRunner, Tools);
+            tool.Pack(Id, Settings);
+        }
+    }
+}
