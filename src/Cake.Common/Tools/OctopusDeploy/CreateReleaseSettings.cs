a23c2ffb888c85aca0a883fad9140fe9fa95a528
trailmax <trailmax1@gmail.com>
2016-09-14 23:05:21 +0100

    Deployment arguments and builder

--- src/Cake.Common/Tools/OctopusDeploy/CreateReleaseSettings.cs
+++ src/Cake.Common/Tools/OctopusDeploy/CreateReleaseSettings.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
 using Cake.Core.IO;
 
@@ -9,9 +10,18 @@ namespace Cake.Common.Tools.OctopusDeploy
 {
     /// <summary>
     /// Contains settings used by <see cref="OctopusDeployReleaseCreator.CreateRelease"/>.
+    /// See Octopus Deploy documentation <see href="http://docs.octopusdeploy.com/display/OD/Creating+releases">here</see>
     /// </summary>
     public sealed class CreateReleaseSettings : OctopusDeploySettings
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateReleaseSettings"/> class.
+        /// </summary>
+        public CreateReleaseSettings()
+        {
+            Variables = new List<KeyValuePair<string, string>>();
+        }
+
         /// <summary>
         /// Gets or sets the release number to use for the new release.
         /// </summary>
@@ -43,9 +53,95 @@ namespace Cake.Common.Tools.OctopusDeploy
         public FilePath ReleaseNotesFile { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether the Ignore Existing flag.
+        /// Gets or sets a value indicating whether to Ignore Existing release flag.
         /// </summary>
         public bool IgnoreExisting { get; set; }
+
+        /// <summary>
+        /// Gets or sets environment to automatically deploy to, e.g., Production.
+        /// </summary>
+        public string DeployTo { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether progress of the deployment should be followed. (Sets --waitfordeployment and --norawlog to true.)
+        /// </summary>
+        public bool ShowProgress { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to force downloading of already installed packages. Default false.
+        /// </summary>
+        public bool ForcePackageDownload { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to wait synchronously for deployment to finish.
+        /// </summary>
+        public bool WaitForDeployment { get; set; }
+
+        /// <summary>
+        /// Gets or sets maximum time (timespan format) that the console session will wait for the deployment to finish (default 00:10:00).
+        /// This will not stop the deployment. Requires WaitForDeployment parameter set.
+        /// </summary>
+        public TimeSpan? DeploymentTimeout { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to cancel the deployment if the deployment timeout is reached(default false).
+        /// </summary>
+        public bool CancelOnTimeout { get; set; }
+
+        /// <summary>
+        /// Gets or sets how much time should elapse between deployment status checks(default 00:00:10).
+        /// </summary>
+        public TimeSpan? DeploymentChecksLeepCycle { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to use Guided Failure mode. If not specified, will use default setting from environment.
+        /// </summary>
+        public bool? GuidedFailure { get; set; }
+
+        /// <summary>
+        /// Gets or sets list of machines names to target in the deployed environment.If not specified all machines in the environment will be considered.
+        /// </summary>
+        public string[] SpecificMachines { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a project is configured to skip packages with already-installed versions, override this setting to force re-deployment (flag, default false).
+        /// </summary>
+        public bool Force { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of steps to be skipped. Takes step names.
+        /// </summary>
+        public string[] SkipSteps { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether print the raw log of failed tasks or not.
+        /// </summary>
+        public bool NoRawLog { get; set; }
+
+        /// <summary>
+        /// Gets or sets a file where to redirect the raw log of failed tasks.
+        /// </summary>
+        public FilePath RawLogFile { get; set; }
+
+        /// <summary>
+        /// Gets or sets values for any prompted variables.
+        /// </summary>
+        public List<KeyValuePair<string, string>> Variables { get; set; }
+
+        /// <summary>
+        /// Gets or sets time at which deployment should start (scheduled deployment), specified as any valid DateTimeOffset format, and assuming the time zone is the current local time zone.
+        /// </summary>
+        public DateTimeOffset? DeployAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets a tenant the deployment will be performed for; specify this argument multiple times to add multiple tenants or use `*` wildcard to deploy to tenants able to deploy.
+        /// </summary>
+        public string[] Tenant { get; set; }
+
+        /// <summary>
+        /// Gets or sets a tenant tags used to match tenants that the deployment will be performed for; specify this argument multiple times to add multiple tenant tags.
+        /// </summary>
+        public string[] TenantTags { get; set; }
 
         /// <summary>
         /// Gets or sets the octopus channel for the new release.
@@ -56,11 +152,6 @@ namespace Cake.Common.Tools.OctopusDeploy
         /// Gets or sets a value indicating whether octopus channel rules should be ignored.
         /// </summary>
         public bool IgnoreChannelRules { get; set; }
-
-        /// <summary>
-        /// Gets or sets the octopus deployment environment for the new release.
-        /// </summary>
-        public string DeployTo { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether progress of the deployment will be shown.
---
+++ src/Cake.Common/Tools/OctopusDeploy/OctopusDeployReleaseDeployer.cs
@@ -0,0 +1,96 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using Cake.Core;
+using Cake.Core.IO;
+using Cake.Core.Tooling;
+
+namespace Cake.Common.Tools.OctopusDeploy
+{
+    /// <summary>
+    /// The Octopus Deploy Release Deploy runner. This class facilitates deploying existing releases in Octopus Deploy.
+    /// </summary>
+    public sealed class OctopusDeployReleaseDeployer : Tool<OctopusDeployReleaseDeploymentSettings>
+    {
+        private readonly ICakeEnvironment _environment;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OctopusDeployReleaseDeployer"/> class.
+        /// </summary>
+        /// <param name="fileSystem">The file system.</param>
+        /// <param name="environment">The environment.</param>
+        /// <param name="processRunner">The process runner.</param>
+        /// <param name="tools">The tool locator.</param>
+        public OctopusDeployReleaseDeployer(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner processRunner, IToolLocator tools)
+            : base(fileSystem, environment, processRunner, tools)
+        {
+            _environment = environment;
+        }
+
+        /// <summary>
+        /// Requests a deployment of a specified release to an environment.
+        /// </summary>
+        /// <param name="server">Octopus Server URL</param>
+        /// <param name="apiKey">The user's API key</param>
+        /// <param name="projectName">Name of the target project</param>
+        /// <param name="deployTo">Environment to deploy to, e.g., Production</param>
+        /// <param name="releaseNumber">Release number to be deployed to</param>
+        /// <param name="settings">Settings for the deployment</param>
+        public void DeployRelease(string server, string apiKey, string projectName, string deployTo, string releaseNumber, OctopusDeployReleaseDeploymentSettings settings)
+        {
+            if (String.IsNullOrEmpty(server))
+            {
+                throw new ArgumentNullException(nameof(server));
+            }
+
+            if (String.IsNullOrEmpty(apiKey))
+            {
+                throw new ArgumentNullException(nameof(apiKey));
+            }
+
+            if (String.IsNullOrEmpty(projectName))
+            {
+                throw new ArgumentNullException(nameof(projectName));
+            }
+
+            if (String.IsNullOrEmpty(deployTo))
+            {
+                throw new ArgumentNullException(nameof(deployTo));
+            }
+
+            if (String.IsNullOrEmpty(releaseNumber))
+            {
+                throw new ArgumentNullException(nameof(releaseNumber));
+            }
+
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            var argumentBuilder = new DeployReleaseArgumentBuilder(server, apiKey, projectName, deployTo, releaseNumber, settings, _environment);
+            Run(settings, argumentBuilder.Get());
+        }
+
+        /// <summary>
+        /// Gets the name of the tool.
+        /// </summary>
+        /// <returns>The name of the tool.</returns>
+        protected override string GetToolName()
+        {
+            return "Octo";
+        }
+
+        /// <summary>
+        /// Gets the possible names of the tool executable.
+        /// </summary>
+        /// <returns>The tool executable name.</returns>
+        protected override IEnumerable<string> GetToolExecutableNames()
+        {
+            return new[] { "Octo.exe" };
+        }
+    }
+}
