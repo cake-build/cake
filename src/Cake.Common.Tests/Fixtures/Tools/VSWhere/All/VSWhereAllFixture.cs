c2429ff8b70a3b31a35d684146938901587100b1
Jamie Phillips <cphill11@gmail.com>
2017-03-04 08:01:12 -0500

    Implemented VSWhere Tool
    The implementation covers the basic use cases and unit tests are included.

---
+++ src/Cake.Common.Tests/Fixtures/Tools/VSWhere/All/VSWhereAllFixture.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Cake.Common.Tools.VSWhere.All;
+
+namespace Cake.Common.Tests.Fixtures.Tools.VSWhere.All
+{
+    internal sealed class VSWhereAllFixture : VSWhereFixture<VSWhereAllSettings>
+    {
+        protected override void RunTool()
+        {
+            var tool = new VSWhereAll(FileSystem, Environment, ProcessRunner, Tools);
+            tool.All(Settings);
+        }
+    }
+}
---
+++ src/Cake.Common.Tests/Unit/Tools/VSWhere/Product/VSWhereProductTests.cs
@@ -0,0 +1,188 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Cake.Common.Tests.Fixtures.Tools.VSWhere.Product;
+using Cake.Testing;
+using Cake.Testing.Xunit;
+using Xunit;
+
+namespace Cake.Common.Tests.Unit.Tools.VSWhere.Product
+{
+    public class VSWhereProductTests
+    {
+        public sealed class TheProductsMethod
+        {
+            [Fact]
+            public void Should_Throw_If_Settings_Are_Null()
+            {
+                // Given
+                var fixture = new VSWhereProductFixture();
+                fixture.Settings = null;
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                Assert.IsArgumentNullException(result, "settings");
+            }
+
+            [Fact]
+            public void Should_Throw_If_VSWhere_Executable_Was_Not_Found()
+            {
+                // Given
+                var fixture = new VSWhereProductFixture();
+                fixture.GivenDefaultToolDoNotExist();
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                Assert.IsCakeException(result, "VSWhere: Could not locate executable.");
+            }
+
+            [Theory]
+            [InlineData("/bin/vswhere/vswhere.exe", "/bin/vswhere/vswhere.exe")]
+            [InlineData("./tools/vswhere/vswhere.exe", "/Working/tools/vswhere/vswhere.exe")]
+            public void Should_Use_VSWhere_Executable_From_Tool_Path_If_Provided(string toolPath, string expected)
+            {
+                // Given
+                var fixture = new VSWhereProductFixture();
+                fixture.Settings.ToolPath = toolPath;
+                fixture.GivenSettingsToolPathExist();
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal(expected, result.Path.FullPath);
+            }
+
+            [WindowsTheory]
+            [InlineData("C:/vswhere/vswhere.exe", "C:/vswhere/vswhere.exe")]
+            public void Should_Use_VSWhere_Executable_From_Tool_Path_If_Provided_On_Windows(string toolPath, string expected)
+            {
+                // Given
+                var fixture = new VSWhereProductFixture();
+                fixture.Settings.ToolPath = toolPath;
+                fixture.GivenSettingsToolPathExist();
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal(expected, result.Path.FullPath);
+            }
+
+            [Fact]
+            public void Should_Throw_If_Process_Was_Not_Started()
+            {
+                // Given
+                var fixture = new VSWhereProductFixture();
+                fixture.GivenProcessCannotStart();
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                Assert.IsCakeException(result, "VSWhere: Process was not started.");
+            }
+
+            [Fact]
+            public void Should_Throw_If_Process_Has_A_Non_Zero_Exit_Code()
+            {
+                // Given
+                var fixture = new VSWhereProductFixture();
+                fixture.GivenProcessExitsWithCode(1);
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                Assert.IsCakeException(result, "VSWhere: Process returned an error (exit code 1).");
+            }
+
+            [Fact]
+            public void Should_Find_VSWhere_Executable_If_Tool_Path_Not_Provided()
+            {
+                // Given
+                var fixture = new VSWhereProductFixture();
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("/Working/tools/vswhere.exe", result.Path.FullPath);
+            }
+
+            [Fact]
+            public void Should_Add_Mandatory_Arguments()
+            {
+                // Given
+                var fixture = new VSWhereProductFixture();
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("-products \"Microsoft.VisualStudio.Product.BuildTools\" -property installationPath -nologo", result.Args);
+            }
+
+            [Fact]
+            public void Should_Add_Version_To_Arguments_If_Set()
+            {
+                // Given
+                var fixture = new VSWhereProductFixture();
+                fixture.Settings.Version = "15";
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("-products \"Microsoft.VisualStudio.Product.BuildTools\" -version \"15\" -property installationPath -nologo", result.Args);
+            }
+
+            [Fact]
+            public void Should_Add_Requires_To_Arguments_If_Set()
+            {
+                // Given
+                var fixture = new VSWhereProductFixture();
+                fixture.Settings.Requires = "Test.Component";
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("-products \"Microsoft.VisualStudio.Product.BuildTools\" -requires \"Test.Component\" -property installationPath -nologo", result.Args);
+            }
+
+            [Fact]
+            public void Should_Not_Include_Property_To_Arguments_If_Set_To_Empty()
+            {
+                // Given
+                var fixture = new VSWhereProductFixture();
+                fixture.Settings.ReturnProperty = string.Empty;
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("-products \"Microsoft.VisualStudio.Product.BuildTools\" -nologo", result.Args);
+            }
+
+            [Fact]
+            public void Should_Not_Include_Products_If_Set_To_Empty()
+            {
+                // Given
+                var fixture = new VSWhereProductFixture();
+                fixture.Settings.Products = string.Empty;
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("-property installationPath -nologo", result.Args);
+            }
+        }
+    }
+}
