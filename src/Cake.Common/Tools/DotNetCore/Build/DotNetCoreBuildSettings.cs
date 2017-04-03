486c613c505f6590d3b2dad3ac44410e502605f0
Jonathan <jonathankuleff@gmail.com>
2017-04-04 06:32:17 +1000

    Updated DotNetCoreBuild inline with Tooling v1.0 #1533

--- src/Cake.Common/Tools/DotNetCore/Build/DotNetCoreBuildSettings.cs
+++ src/Cake.Common/Tools/DotNetCore/Build/DotNetCoreBuildSettings.cs
@@ -12,11 +12,6 @@ namespace Cake.Common.Tools.DotNetCore.Build
     public sealed class DotNetCoreBuildSettings : DotNetCoreSettings
     {
         /// <summary>
-        /// Gets or sets the directory in which to place temporary outputs.
-        /// </summary>
-        public DirectoryPath BuildBasePath { get; set; }
-
-        /// <summary>
         /// Gets or sets the output directory.
         /// </summary>
         public DirectoryPath OutputDirectory { get; set; }
@@ -40,11 +35,6 @@ namespace Cake.Common.Tools.DotNetCore.Build
         /// Gets or sets the value that defines what `*` should be replaced with in version field in project.json.
         /// </summary>
         public string VersionSuffix { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether to print the incremental safety checks that prevent incremental compilation.
-        /// </summary>
-        public bool BuildProfile { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether to mark the build as unsafe for incrementality.
