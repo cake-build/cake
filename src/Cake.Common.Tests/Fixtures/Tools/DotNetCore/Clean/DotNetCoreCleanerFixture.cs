56d91913f29739f7c94bc35c8837a3366dc30543
Jonathan <jonathankuleff@gmail.com>
2017-04-04 06:34:09 +1000

    Added support for DotNetCoreClean inline with Tooling v1.0 #1533

---
+++ src/Cake.Common.Tests/Fixtures/Tools/DotNetCore/Clean/DotNetCoreCleanerFixture.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Cake.Common.Tools.DotNetCore.Clean;
+
+namespace Cake.Common.Tests.Fixtures.Tools.DotNetCore.Clean
+{
+    internal sealed class DotNetCoreCleanerFixture : DotNetCoreFixture<DotNetCoreCleanSettings>
+    {
+        public string Project { get; set; }
+
+        protected override void RunTool()
+        {
+            var tool = new DotNetCoreCleaner(FileSystem, Environment, ProcessRunner, Tools);
+            tool.Clean(Project, Settings);
+        }
+    }
+}
