e269dfdebf84fea93c752a8e24d746b55091a27b
Mattias Karlsson <matkar@wcom.se>
2017-09-16 21:53:41 +0200

    (build) Updated Cake tool to version 0.22.1

--- build.ps1
+++ build.ps1
@@ -31,7 +31,7 @@ Param(
     [string[]]$ScriptArgs
 )
 
-$CakeVersion = "0.22.0"
+$CakeVersion = "0.22.1"
 $DotNetChannel = "preview";
 $DotNetVersion = "1.0.4";
 $DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
