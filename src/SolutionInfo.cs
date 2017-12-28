a346c8fd2fb5d5d9fa8e8e36ab8c5ade6db07c5b
Mattias Karlsson <matkar@wcom.se>
2017-12-29 00:21:32 +0100

    (build) Updated version and release notes.

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -10,7 +10,7 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Cake")]
-[assembly: AssemblyVersion("0.23.0.0")]
-[assembly: AssemblyFileVersion("0.23.0.0")]
-[assembly: AssemblyInformationalVersion("0.23.0-beta.1+0.Branch.release/0.23.0.Sha.7928d8cb99b1da79f73e45dbcbfffb3adde18714")]
+[assembly: AssemblyVersion("0.24.0.0")]
+[assembly: AssemblyFileVersion("0.24.0.0")]
+[assembly: AssemblyInformationalVersion("0.24.0-beta.1+0.Branch.release/0.24.0.Sha.50724a9778491ecaa7becdf919241ed81ee7fa5b")]
 [assembly: AssemblyCopyright("Copyright (c) .NET Foundation and Contributors")]
