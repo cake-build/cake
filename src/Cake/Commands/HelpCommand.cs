baeb8176ff2d4f6d63d6fc64a0d0905d510dd5a1
Roma Marusyk <romamarusyk@gmail.com>
2018-02-14 11:53:33 +0200

    (GH-2036
    ) Outputted how to invoke the help command when an error occurred.
    
    * Fixes #2036

--- src/Cake/Commands/HelpCommand.cs
+++ src/Cake/Commands/HelpCommand.cs
@@ -25,24 +25,31 @@ namespace Cake.Commands
 
         public bool Execute(CakeOptions options)
         {
-            _console.WriteLine();
-            _console.WriteLine("Usage: Cake.exe [script] [--verbosity=value]");
-            _console.WriteLine("                [--showdescription] [--dryrun] [..]");
-            _console.WriteLine();
-            _console.WriteLine("Example: Cake.exe");
-            _console.WriteLine("Example: Cake.exe build.cake --verbosity=quiet");
-            _console.WriteLine("Example: Cake.exe build.cake --showdescription");
-            _console.WriteLine();
-            _console.WriteLine("Options:");
-            _console.WriteLine("    --verbosity=value    Specifies the amount of information to be displayed.");
-            _console.WriteLine("                         ({0})", string.Join(", ", Enum.GetNames(typeof(Verbosity))));
-            _console.WriteLine("    --debug              Performs a debug.");
-            _console.WriteLine("    --showdescription    Shows description about tasks.");
-            _console.WriteLine("    --dryrun             Performs a dry run.");
-            _console.WriteLine("    --version            Displays version information.");
-            _console.WriteLine("    --help               Displays usage information.");
-            _console.WriteLine();
-
+            if (options.HasError)
+            {
+                _console.WriteLine("For usage, use parameter --help");
+            }
+            else
+            {
+                _console.WriteLine();
+                _console.WriteLine("Usage: Cake.exe [script] [--verbosity=value]");
+                _console.WriteLine("                [--showdescription] [--dryrun] [..]");
+                _console.WriteLine();
+                _console.WriteLine("Example: Cake.exe");
+                _console.WriteLine("Example: Cake.exe build.cake --verbosity=quiet");
+                _console.WriteLine("Example: Cake.exe build.cake --showdescription");
+                _console.WriteLine();
+                _console.WriteLine("Options:");
+                _console.WriteLine("    --verbosity=value    Specifies the amount of information to be displayed.");
+                _console.WriteLine("                         ({0})",
+                    string.Join(", ", Enum.GetNames(typeof(Verbosity))));
+                _console.WriteLine("    --debug              Performs a debug.");
+                _console.WriteLine("    --showdescription    Shows description about tasks.");
+                _console.WriteLine("    --dryrun             Performs a dry run.");
+                _console.WriteLine("    --version            Displays version information.");
+                _console.WriteLine("    --help               Displays usage information.");
+                _console.WriteLine();
+            }
             return true;
         }
     }
