a7668ec13f518f19303c4e1f2b532964fb302629
Gary Ewan Park <gep13@gep13.co.uk>
2018-08-16 18:02:03 +0100

    (GH-2232) Document exclusive argument

--- src/Cake/Commands/HelpCommand.cs
+++ src/Cake/Commands/HelpCommand.cs
@@ -46,6 +46,7 @@ namespace Cake.Commands
                 _console.WriteLine("    --debug              Performs a debug.");
                 _console.WriteLine("    --showdescription    Shows description about tasks.");
                 _console.WriteLine("    --dryrun             Performs a dry run.");
+                _console.WriteLine("    --exclusive          Execute a single task without any dependencies.");
                 _console.WriteLine("    --version            Displays version information.");
                 _console.WriteLine("    --help               Displays usage information.");
                 _console.WriteLine();
