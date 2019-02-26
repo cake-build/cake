2edec355dd1c17d093d96ef564ca94b155463c15
DESKTOP-340JJAF\David <muellerdavid4@gmail.com>
2019-02-27 00:32:24 +0100

    Expose ICakeConfiguration on CakeContext

--- src/Cake.Core/ICakeContext.cs
+++ src/Cake.Core/ICakeContext.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Cake.Core.Configuration;
 using Cake.Core.Diagnostics;
 using Cake.Core.IO;
 using Cake.Core.Tooling;
@@ -65,5 +66,10 @@ namespace Cake.Core
         /// Gets the data context resolver.
         /// </summary>
         ICakeDataResolver Data { get; }
+
+        /// <summary>
+        /// Gets the cake configuration.
+        /// </summary>
+        ICakeConfiguration Configuration { get; }
     }
 }
