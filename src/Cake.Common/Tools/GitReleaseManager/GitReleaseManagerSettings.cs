bb31672cca4dc3c6484125ae2cf835a2b9fce7a0
Gary Ewan Park <gep13@gep13.co.uk>
2020-01-17 00:14:45 -0800

    (GH-2696) Use common base class for settings
    Not entirely sure what this wasn't done initially, but I think it makes
    sense to apply this refactoring now.  Makes it easier to add the three
    new common options.

---
+++ src/Cake.Common/Tools/GitReleaseManager/GitReleaseManagerSettings.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Cake.Core.IO;
+using Cake.Core.Tooling;
+
+namespace Cake.Common.Tools.GitReleaseManager
+{
+    /// <summary>
+    /// Contains settings used by <see cref="GitReleaseManagerSettings"/>
+    /// </summary>
+    public class GitReleaseManagerSettings : ToolSettings
+    {
+        /// <summary>
+        /// Gets or sets the path on which GitReleaseManager should be executed.
+        /// </summary>
+        public DirectoryPath TargetDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the path to the GitReleaseManager log file.
+        /// </summary>
+        public FilePath LogFilePath { get; set; }
+    }
+}
