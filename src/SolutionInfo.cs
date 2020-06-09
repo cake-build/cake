c44560730b90e26d35a145650a325dfa49438f0f
Gary Ewan Park <gep13@gep13.co.uk>
2020-06-09 19:56:38 +0100

    (build) Updated version and release notes.

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -10,7 +10,7 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Cake")]
-[assembly: AssemblyVersion("0.38.1.0")]
-[assembly: AssemblyFileVersion("0.38.1.0")]
-[assembly: AssemblyInformationalVersion("0.38.1-beta.1+0.Branch.release-0.38.1.Sha.3e24a52e2b2f3285016a31c2dd3e08df94a52d7b")]
+[assembly: AssemblyVersion("0.38.2.0")]
+[assembly: AssemblyFileVersion("0.38.2.0")]
+[assembly: AssemblyInformationalVersion("0.38.2-beta.1+24.Branch.hotfix-0.38.2.Sha.15a8ce20fd8845352cf1123a521408b7e228d271")]
 [assembly: AssemblyCopyright("Copyright (c) .NET Foundation and Contributors")]
