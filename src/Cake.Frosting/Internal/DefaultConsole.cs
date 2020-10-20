a384939ee9a4994e94ec5337772247631d2170e9
Roma Marusyk <romamarusyk@gmail.com>
2020-10-20 21:43:37 +0300

    Cake.Frosting. Remove unnecessary documentation and replace it with <inheritdoc />

--- src/Cake.Frosting/Internal/DefaultConsole.cs
+++ src/Cake.Frosting/Internal/DefaultConsole.cs
@@ -9,80 +9,48 @@ namespace Cake.Frosting.Internal
 {
     internal sealed class DefaultConsole : IConsole
     {
-        /// <summary>
-        /// Gets or sets the foreground color.
-        /// </summary>
-        /// <value>The foreground color.</value>
+        /// <inheritdoc/>
         public ConsoleColor ForegroundColor
         {
             get => Console.ForegroundColor;
             set => Console.ForegroundColor = value;
         }
 
-        /// <summary>
-        /// Gets or sets the background color.
-        /// </summary>
-        /// <value>The background color.</value>
+        /// <inheritdoc/>
         public ConsoleColor BackgroundColor
         {
             get => Console.BackgroundColor;
             set => Console.BackgroundColor = value;
         }
 
-        /// <summary>
-        /// Gets a value indicating whether gets if console supports ANSI escape codes.
-        /// </summary>
+        /// <inheritdoc/>
         public bool SupportAnsiEscapeCodes => false;
 
-        /// <summary>
-        /// Writes the text representation of the specified array of objects to the
-        /// console output using the specified format information.
-        /// </summary>
-        /// <param name="format">A composite format string.</param>
-        /// <param name="arg">An array of objects to write using format.</param>
+        /// <inheritdoc/>
         public void Write(string format, params object[] arg)
         {
             Console.Write(format, arg);
         }
 
-        /// <summary>
-        /// Writes the text representation of the specified array of objects, followed
-        /// by the current line terminator, to the console output using the specified
-        /// format information.
-        /// </summary>
-        /// <param name="format">A composite format string.</param>
-        /// <param name="arg">An array of objects to write using format.</param>
+        /// <inheritdoc/>
         public void WriteLine(string format, params object[] arg)
         {
             Console.WriteLine(format, arg);
         }
 
-        /// <summary>
-        /// Writes the text representation of the specified array of objects to the
-        /// console error output using the specified format information.
-        /// </summary>
-        /// <param name="format">A composite format string.</param>
-        /// <param name="arg">An array of objects to write using format.</param>
+        /// <inheritdoc/>
         public void WriteError(string format, params object[] arg)
         {
             Console.Error.Write(format, arg);
         }
 
-        /// <summary>
-        /// Writes the text representation of the specified array of objects, followed
-        /// by the current line terminator, to the console error output using the
-        /// specified format information.
-        /// </summary>
-        /// <param name="format">A composite format string.</param>
-        /// <param name="arg">An array of objects to write using format.</param>
+        /// <inheritdoc/>
         public void WriteErrorLine(string format, params object[] arg)
         {
             Console.Error.WriteLine(format, arg);
         }
 
-        /// <summary>
-        /// Sets the foreground and background console colors to their defaults.
-        /// </summary>
+        /// <inheritdoc/>
         public void ResetColor()
         {
             Console.ResetColor();
