748814a37979be7fed58fc6a3e732bdb18f9f096
Pascal Berger <pascal.berger@gmail.com>
2020-11-27 13:27:17 +0100

    (GH-2956) Remove build.config from Frosting template

--- src/Cake.Frosting.Template/build.ps1
+++ src/Cake.Frosting.Template/build.ps1
@@ -1,27 +1,8 @@
 #!/usr/bin/env pwsh
 $DotNetInstallerUri = 'https://dot.net/v1/dotnet-install.ps1';
-$DotNetUnixInstallerUri = 'https://dot.net/v1/dotnet-install.sh'
-$DotNetChannel = 'LTS'
 $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
-
-[string] $DotNetVersion= ''
-foreach($line in Get-Content (Join-Path $PSScriptRoot 'build.config'))
-{
-  if ($line -like 'DOTNET_VERSION=*') {
-      $DotNetVersion =$line.SubString(15)
-  }
-}
-
-
-if ([string]::IsNullOrEmpty($DotNetVersion)) {
-    'Failed to parse .NET Core SDK Version'
-    exit 1
-}
-
-$DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
 
 # Make sure tools folder exists
-$PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 $ToolPath = Join-Path $PSScriptRoot "tools"
 if (!(Test-Path $ToolPath)) {
     Write-Verbose "Creating tools directory..."
@@ -42,25 +23,17 @@ Function Remove-PathVariable([string]$VariableToRemove)
   [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join(';', $newItems), "Process")
 }
 
-# Get .NET Core CLI path if installed.
-$FoundDotNetCliVersion = $null;
-if (Get-Command dotnet -ErrorAction SilentlyContinue) {
-    $FoundDotNetCliVersion = dotnet --version;
+$InstallPath = Join-Path $PSScriptRoot ".dotnet"
+if (!(Test-Path $InstallPath)) {
+    mkdir -Force $InstallPath | Out-Null;
 }
+(New-Object System.Net.WebClient).DownloadFile($DotNetInstallerUri, "$InstallPath\dotnet-install.ps1");
+& $InstallPath\dotnet-install.ps1 -JSonFile global.json -InstallDir $InstallPath;
 
-if($FoundDotNetCliVersion -ne $DotNetVersion) {
-    $InstallPath = Join-Path $PSScriptRoot ".dotnet"
-    if (!(Test-Path $InstallPath)) {
-        mkdir -Force $InstallPath | Out-Null;
-    }
-    (New-Object System.Net.WebClient).DownloadFile($DotNetInstallerUri, "$InstallPath\dotnet-install.ps1");
-    & $InstallPath\dotnet-install.ps1 -Version $DotNetVersion -InstallDir $InstallPath;
-
-    Remove-PathVariable "$InstallPath"
-    $env:PATH = "$InstallPath;$env:PATH"
-    $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-    $env:DOTNET_CLI_TELEMETRY_OPTOUT=1
-}
+Remove-PathVariable "$InstallPath"
+$env:PATH = "$InstallPath;$env:PATH"
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+$env:DOTNET_CLI_TELEMETRY_OPTOUT=1
 
 ###########################################################################
 # RUN BUILD SCRIPT
--- src/Cake.Frosting.Template/build.sh
+++ src/Cake.Frosting.Template/build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-# Define varibles
+# Define variables
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source $SCRIPT_DIR/build.config
-
-if [ "$DOTNET_VERSION" = "" ]; then
-    echo "An error occured while parsing .NET Core SDK version."
-    exit 1
-fi
 
 ###########################################################################
 # INSTALL .NET CORE CLI
@@ -17,18 +11,14 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
 export DOTNET_ROLL_FORWARD_ON_NO_CANDIDATE_FX=2
 
-DOTNET_INSTALLED_VERSION=$(dotnet --version 2>&1)
-
-if [ "$DOTNET_VERSION" != "$DOTNET_INSTALLED_VERSION" ]; then
-    echo "Installing .NET CLI..."
-    if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
-      mkdir "$SCRIPT_DIR/.dotnet"
-    fi
-    curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://dot.net/v1/dotnet-install.sh
-    bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version $DOTNET_VERSION --install-dir .dotnet --no-path
-    export PATH="$SCRIPT_DIR/.dotnet":$PATH
-    export DOTNET_ROOT="$SCRIPT_DIR/.dotnet"
+echo "Installing .NET CLI..."
+if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
+  mkdir "$SCRIPT_DIR/.dotnet"
 fi
+curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://dot.net/v1/dotnet-install.sh
+bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --jsonfile "$SCRIPT_DIR/global.json"  --install-dir .dotnet --no-path
+export PATH="$SCRIPT_DIR/.dotnet":$PATH
+export DOTNET_ROOT="$SCRIPT_DIR/.dotnet"
 
 ###########################################################################
 # RUN BUILD SCRIPT
