de4ccb777a5fe78e119aacd9f0823b800d17d5ca
Pascal Berger <pascal.berger@gmail.com>
2021-01-03 00:10:29 +0100

    Remove unused parameter

--- src/Cake.Frosting/Internal/FrostingEngine.cs
+++ src/Cake.Frosting/Internal/FrostingEngine.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using Cake.Core;
 using Cake.Core.Diagnostics;
-using Cake.Core.IO;
 using Cake.Core.Scripting;
 
 namespace Cake.Frosting.Internal
@@ -14,7 +13,7 @@ namespace Cake.Frosting.Internal
     internal interface IFrostingEngine
     {
         ExecutionSettings Settings { get; }
-        CakeReport Run(string target, Verbosity verbosity, DirectoryPath workingDirectory);
+        CakeReport Run(string target, Verbosity verbosity);
     }
 
     internal abstract class FrostingEngine<THost> : IFrostingEngine
@@ -52,7 +51,7 @@ namespace Cake.Frosting.Internal
             _tasks = new List<IFrostingTask>(tasks ?? Array.Empty<IFrostingTask>());
         }
 
-        public CakeReport Run(string target, Verbosity verbosity, DirectoryPath workingDirectory)
+        public CakeReport Run(string target, Verbosity verbosity)
         {
             _log.Verbosity = verbosity;
 
