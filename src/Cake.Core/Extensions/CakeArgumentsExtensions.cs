4cc4f7163876b7cdc8f2093e35b6ef00199a41d7
Patrik Svensson <patrik@patriksvensson.se>
2021-01-23 23:53:59 +0100

    Add ICakeArguments.GetArgument extension
    Closes #3029

---
+++ src/Cake.Core/Extensions/CakeArgumentsExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Cake.Core
+{
+    /// <summary>
+    /// Contains extension methods for <see cref="ICakeArguments"/>.
+    /// </summary>
+    public static class CakeArgumentsExtensions
+    {
+        /// <summary>
+        /// Gets the value for an argument.
+        /// </summary>
+        /// <remarks>
+        /// If multiple arguments with the same name are
+        /// specified, the last argument value is returned.
+        /// </remarks>
+        /// <param name="arguments">The arguments.</param>
+        /// <param name="name">The argument name.</param>
+        /// <returns>The argument value.</returns>
+        public static string GetArgument(this ICakeArguments arguments, string name)
+        {
+            return arguments.GetArguments(name).LastOrDefault();
+        }
+    }
+}
