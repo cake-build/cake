170639d55a802e9a27644932e258ff033d4ffe96
Chris Blyth <blythmeister@gmail.com>
2021-02-10 12:23:53 +0000

    Add GetArgumentOrDefault to extensions

--- src/Cake.Core.Tests/Unit/CakeArgumentsTests.cs
+++ src/Cake.Core.Tests/Unit/CakeArgumentsTests.cs
@@ -8,7 +8,7 @@ namespace Cake.Core.Tests.Unit
     public sealed class CakeArgumentsTests
     {
         [Fact]
-        public void Should_Return_Null_If_Argument_Is_Missing()
+        public void GetArgument_Should_Return_Null_If_Argument_Is_Missing()
         {
             // Given
             var arguments = new CakeArguments(new List<Tuple<string, string>>
@@ -25,7 +25,7 @@ namespace Cake.Core.Tests.Unit
         }
 
         [Fact]
-        public void Should_Return_Argument_If_One_With_The_Specified_Name_Exist()
+        public void GetArgument_Should_Return_Argument_If_One_With_The_Specified_Name_Exist()
         {
             // Given
             var arguments = new CakeArguments(new List<Tuple<string, string>>
@@ -42,7 +42,7 @@ namespace Cake.Core.Tests.Unit
         }
 
         [Fact]
-        public void Should_Return_The_Last_Argument_If_Multiple_Ones_With_The_Same_Name_Exist()
+        public void GetArgument_Should_Return_The_Last_Argument_If_Multiple_Ones_With_The_Same_Name_Exist()
         {
             // Given
             var arguments = new CakeArguments(new List<Tuple<string, string>>
@@ -57,5 +57,57 @@ namespace Cake.Core.Tests.Unit
             // Then
             Assert.Equal("BAZ", result);
         }
+
+        [Fact]
+        public void GetArgumentOrDefault_Should_Return_Default_If_Argument_Is_Missing()
+        {
+            // Given
+            var arguments = new CakeArguments(new List<Tuple<string, string>>
+            {
+                Tuple.Create("FOO", "BAR"),
+                Tuple.Create("BAZ", "CORGI"),
+            }.ToLookup(x => x.Item1, x => x.Item2));
+
+            // When
+            var result = arguments.GetArgumentOrDefault("WALDO", "DefaultValue");
+
+            // Then
+            Assert.Equal("DefaultValue", result);
+        }
+
+        [Fact]
+        public void GetArgumentOrDefault_Should_Return_Argument_If_One_With_The_Specified_Name_Exist()
+        {
+            // Given
+            var arguments = new CakeArguments(new List<Tuple<string, string>>
+            {
+                Tuple.Create("FOO", "BAR"),
+                Tuple.Create("BAZ", "CORGI"),
+            }.ToLookup(x => x.Item1, x => x.Item2));
+
+            // When
+            var result = arguments.GetArgumentOrDefault("BAZ", "DefaultValue");
+
+            // Then
+            Assert.Equal("CORGI", result);
+        }
+
+        [Fact]
+        public void GetArgumentOrDefault_Should_Return_The_Last_Argument_If_Multiple_Ones_With_The_Same_Name_Exist()
+        {
+            // Given
+            var arguments = new CakeArguments(new List<Tuple<string, string>>
+            {
+                Tuple.Create("FOO", "BAR"),
+                Tuple.Create("FOO", "BAZ"),
+            }.ToLookup(x => x.Item1, x => x.Item2));
+
+            // When
+            var result = arguments.GetArgumentOrDefault("FOO", "DefaultValue");
+
+            // Then
+            Assert.Equal("BAZ", result);
+        }
+
     }
 }
--- src/Cake.Core/Extensions/CakeArgumentsExtensions.cs
+++ src/Cake.Core/Extensions/CakeArgumentsExtensions.cs
@@ -22,5 +22,21 @@ namespace Cake.Core
         {
             return arguments.GetArguments(name).LastOrDefault();
         }
+
+        /// <summary>
+        /// Gets the value for an argument or uses default value.
+        /// </summary>
+        /// <remarks>
+        /// If multiple arguments with the same name are
+        /// specified, the last argument value is returned.
+        /// </remarks>
+        /// <param name="arguments">The arguments.</param>
+        /// <param name="name">The argument name.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>The argument value.</returns>
+        public static string GetArgumentOrDefault(this ICakeArguments arguments, string name, string defaultValue)
+        {
+            return arguments.GetArgument(name) ?? defaultValue;
+        }
     }
 }
