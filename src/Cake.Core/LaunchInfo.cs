119367688daf4528816b288d7432735c14ac8c86
Roman Marusyk <romamarusyk@gmail.com>
2021-03-05 02:59:39 +0200

    Fix using

--- src/Cake.Core/LaunchInfo.cs
+++ src/Cake.Core/LaunchInfo.cs
@@ -2,8 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Cake.Core.IO;
 using System;
+using Cake.Core.IO;
 
 namespace Cake.Core
 {
