ca37f866a14ea34d794a7c1bbe694aa992dea84f
Roman Marusyk <romamarusyk@gmail.com>
2021-03-05 03:09:16 +0200

    Add unit test

--- src/Cake.Core.Tests/Unit/CakeEnvironmentTests.cs
+++ src/Cake.Core.Tests/Unit/CakeEnvironmentTests.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using Cake.Core.IO;
+using Cake.Testing;
 using NSubstitute;
 using Xunit;
 
@@ -12,6 +13,22 @@ namespace Cake.Core.Tests.Unit
     {
         public sealed class LaunchDirectoryProperty
         {
+            [Theory]
+            [InlineData(PlatformFamily.Linux)]
+            [InlineData(PlatformFamily.OSX)]
+            [InlineData(PlatformFamily.Windows)]
+            public void Should_Not_Changes_When_WorkingDirectory_Changed(PlatformFamily family)
+            {
+                // Given
+                var context = Substitute.For<ICakeContext>();
+                context.Environment.Returns(new FakeEnvironment(family));
+
+                // When
+                context.Environment.WorkingDirectory = "/build";
+
+                // Then
+                Assert.NotEqual(context.Environment.WorkingDirectory, context.Environment.LaunchDirectory);
+            }
         }
     }
 }
