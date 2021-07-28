f4f83797fd4247825d4876858e5f7877491b2dbf
Manasvi Goyal <55101825+ManasviGoyal@users.noreply.github.com>
2021-07-29 05:13:11 +0530

    Update ReleaseNotesParser.cs

--- src/Cake.Common/ReleaseNotesParser.cs
+++ src/Cake.Common/ReleaseNotesParser.cs
@@ -22,7 +22,7 @@ namespace Cake.Common
         /// </summary>
         public ReleaseNotesParser()
         {
-            _versionRegex = new RegexRegex(@"^(?<Version>\d+(\s*\.\s*\d+){0,3})(?<Release>-[a-z][0-9a-z-]*)?$");
+            _versionRegex = new RegexRegex(@"^(?<Version>\d+(\s*\.\s*\d+){0,3})(?<Release>-[a-z][0-9a-z-]*)?");
         }
 
         /// <summary>
