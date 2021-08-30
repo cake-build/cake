96056ffd0734ef3694f9982c94d2532b8ef3b86f
Sean Rowland <srowla94@gmail.com>
2021-08-30 16:30:54 -0400

    (GH-2104) Add custom exit code to CakeException * fixes #2104

---
+++ src/Cake.Core.Tests/Exceptions/CustomeExitCodeTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cake.Core.Tests.Exceptions
+{
+    public class CustomeExitCodeTests
+    {
+        [Fact]
+        public void Should_Return_Default_ExitCode()
+        {
+            var exception = new CakeException();
+            Assert.Equal(1, exception.ExitCode);
+        }
+
+        [Fact]
+        public void Should_Return_Custom_ExitCode()
+        {
+            var exception = new CakeException(5);
+            Assert.Equal(5, exception.ExitCode);
+        }
+    }
+}
