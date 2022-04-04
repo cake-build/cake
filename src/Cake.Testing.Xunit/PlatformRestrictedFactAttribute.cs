1cb1692534a2424a8ef1d7acde786ecb4db0c6fb
Nils Andresen <nils@nils-andresen.de>
2022-04-04 23:22:22 +0200

    (#3858, #3859) Made PathCollapser aware of different formats
    especially different calculations of the root of a path
    for windows, non-windows and UNC like paths.
    
    To this end, I re-structured the unit test. There were no test
    removed that previously existed, but only new tests added.
    
    Also, instead of joining together the path with one specific
    separator ('/'), I re-used the existing separator of the original
    path. (This was the main-fix for #3859.)

---
+++ src/Cake.Testing.Xunit/PlatformRestrictedFactAttribute.cs
@@ -0,0 +1,52 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Cake.Core;
+using Cake.Core.Polyfill;
+using Xunit;
+
+namespace Cake.Testing.Xunit
+{
+    public abstract class PlatformRestrictedFactAttribute : FactAttribute
+    {
+        private static readonly PlatformFamily _family;
+        private string _skip;
+
+        static PlatformRestrictedFactAttribute()
+        {
+            _family = EnvironmentHelper.GetPlatformFamily();
+        }
+
+        protected PlatformRestrictedFactAttribute(
+            PlatformFamily requiredFamily,
+            bool invert,
+            string reason = null)
+        {
+            if ((requiredFamily != _family) ^ invert)
+            {
+                if (string.IsNullOrEmpty(reason))
+                {
+                    var platformName = Enum.GetName(typeof(PlatformFamily), requiredFamily);
+                    if (invert)
+                    {
+                        platformName = $"Non-{platformName}";
+                    }
+
+                    reason = $"{platformName} test.";
+                }
+
+                Reason = reason;
+            }
+        }
+
+        private string Reason { get; }
+
+        public override string Skip
+        {
+            get => _skip ?? Reason;
+            set => _skip = value;
+        }
+    }
+}
