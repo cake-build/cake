26ec88be2bf3d515b569aec4888a7620273e24e4
Jon Kjar <jkjar@daasolutions.com>
2022-06-08 18:33:24 -0600

    Added null check in InfoFeature and tests in versionfeature and infofeature.

--- src/Cake.Tests/Unit/Features/InfoFeatureTests.cs
+++ src/Cake.Tests/Unit/Features/InfoFeatureTests.cs
@@ -22,5 +22,21 @@ namespace Cake.Tests.Unit.Features
             Assert.Contains("Version: 1.2.3", console.Messages);
             Assert.Contains("Details: 3.2.1", console.Messages);
         }
+
+
+        [Fact]
+        public void Should_Throw_If_Console_Is_Null()
+        {
+            // Given
+            var resolver = new FakeVersionResolver("1.2.3", "3.2.1");
+            var feature = new InfoFeature(resolver);
+
+            // When
+            var result = Record.Exception(() =>
+                     feature.Run(null));
+
+            // Then
+            AssertEx.IsArgumentNullException(result, "console");
+        }
     }
 }
