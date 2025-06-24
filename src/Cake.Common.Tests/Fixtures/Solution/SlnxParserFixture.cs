65f7b097d29136a3d94a68490d9637418d8c093c
Someoneeeeeeee <hfgilani06@gmail.com>
2025-06-24 14:56:28 +1000

    Added Unit tests and dummy solution for testing

---
+++ src/Cake.Common.Tests/Fixtures/Solution/SlnxParserFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Cake.Core.IO;
+using Cake.Testing;
+
+namespace Cake.Common.Tests.Fixtures.Solution
+{
+    internal sealed class SlnxParserFixture
+    {
+        public FakeEnvironment Environment { get; set; }
+
+        public FakeFileSystem FileSystem { get; set; }
+
+        public SlnxParserFixture()
+        {
+            var environment = FakeEnvironment.CreateUnixEnvironment();
+            Environment = environment;
+            var fileSystem = new FakeFileSystem(environment);
+            FileSystem = fileSystem;
+        }
+
+        public static FilePath WithSolutionFile(string slnxContent)
+        {
+            var tempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), $"dummySolution_{Guid.NewGuid()}.slnx");
+            System.IO.File.WriteAllText(tempPath, slnxContent);
+            return new FilePath(tempPath);
+        }
+    }
+}
